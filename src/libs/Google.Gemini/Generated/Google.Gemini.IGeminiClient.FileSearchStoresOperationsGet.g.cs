a2cad75aad1dca2a4a79d6f28384d0819b0f6c53
Konstantin S. <havendv@gmail.com>
2026-03-22 19:25:56 +0400

    feat: Updated OpenAPI spec (#110)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Google.Gemini/Generated/Google.Gemini.IGeminiClient.FileSearchStoresOperationsGet.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.IGeminiClient.FileSearchStoresOperationsGet.g.cs
@@ -7,13 +7,13 @@ namespace Google.Gemini
         /// <summary>
         /// Gets the latest state of a long-running operation. Clients can use this method to poll the operation result at intervals as recommended by the API service.
         /// </summary>
-        /// <param name="fileSearchStoresId"></param>
         /// <param name="operationsId"></param>
+        /// <param name="fileSearchStoresId"></param>
         /// <param name="cancellationToken">The token to cancel the operation with</param>
         /// <exception cref="global::Google.Gemini.ApiException"></exception>
         global::System.Threading.Tasks.Task<global::Google.Gemini.Operation> FileSearchStoresOperationsGetAsync(
-            string fileSearchStoresId,
             string operationsId,
+            string fileSearchStoresId,
             global::System.Threading.CancellationToken cancellationToken = default);
     }
 }
