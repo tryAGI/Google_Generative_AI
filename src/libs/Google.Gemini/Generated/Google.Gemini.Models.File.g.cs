d4c0d942caa6b94f2fb9e83259d6a850df2de618
Konstantin S. <havendv@gmail.com>
2026-05-07 18:05:13 +0400

    feat: Updated OpenAPI spec (#368)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.File.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.File.g.cs
@@ -9,65 +9,16 @@ namespace Google.Gemini
     public sealed partial class File
     {
         /// <summary>
-        /// Output only. MIME type of the file.<br/>
-        /// Included only in responses
+        /// Optional. The human-readable display name for the `File`. The display name must be no more than 512 characters in length, including spaces. Example: "Welcome Image"
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("mimeType")]
-        public string? MimeType { get; set; }
-
-        /// <summary>
-        /// Output only. The timestamp of when the `File` was created.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("createTime")]
-        public string? CreateTime { get; set; }
-
-        /// <summary>
-        /// Output only. The timestamp of when the `File` will be deleted. Only set if the `File` is scheduled to expire.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("expirationTime")]
-        public string? ExpirationTime { get; set; }
-
-        /// <summary>
-        /// Source of the File.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("source")]
-        [global::System.Text.Json.Serialization.JsonConverter(typeof(global::Google.Gemini.JsonConverters.FileSourceJsonConverter))]
-        public global::Google.Gemini.FileSource? Source { get; set; }
-
-        /// <summary>
-        /// Output only. Size of the file in bytes.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("sizeBytes")]
-        public string? SizeBytes { get; set; }
-
-        /// <summary>
-        /// Output only. The download uri of the `File`.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("downloadUri")]
-        public string? DownloadUri { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("displayName")]
+        public string? DisplayName { get; set; }
 
         /// <summary>
         /// The `Status` type defines a logical error model that is suitable for different programming environments, including REST APIs and RPC APIs. It is used by [gRPC](https://github.com/grpc). Each `Status` message contains three pieces of data: error code, error message, and error details. You can find out more about this error model and how to work with it in the [API Design Guide](https://cloud.google.com/apis/design/errors).
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("error")]
         public global::Google.Gemini.Status? Error { get; set; }
-
-        /// <summary>
-        /// Immutable. Identifier. The `File` resource name. The ID (name excluding the "files/" prefix) can contain up to 40 characters that are lowercase alphanumeric or dashes (-). The ID cannot start or end with a dash. If the name is empty on create, a unique name will be generated. Example: `files/123-456`
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("name")]
-        public string? Name { get; set; }
-
-        /// <summary>
-        /// Output only. The uri of the `File`.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("uri")]
-        public string? Uri { get; set; }
 
         /// <summary>
         /// Output only. SHA-256 hash of the uploaded bytes.<br/>
@@ -77,6 +28,26 @@ namespace Google.Gemini
         public byte[]? Sha256Hash { get; set; }
 
         /// <summary>
+        /// Output only. The timestamp of when the `File` was created.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("createTime")]
+        public string? CreateTime { get; set; }
+
+        /// <summary>
+        /// Output only. MIME type of the file.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("mimeType")]
+        public string? MimeType { get; set; }
+
+        /// <summary>
+        /// Immutable. Identifier. The `File` resource name. The ID (name excluding the "files/" prefix) can contain up to 40 characters that are lowercase alphanumeric or dashes (-). The ID cannot start or end with a dash. If the name is empty on create, a unique name will be generated. Example: `files/123-456`
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        /// <summary>
         /// Output only. The timestamp of when the `File` was last updated.<br/>
         /// Included only in responses
         /// </summary>
@@ -84,10 +55,25 @@ namespace Google.Gemini
         public string? UpdateTime { get; set; }
 
         /// <summary>
-        /// Metadata for a video `File`.
+        /// Source of the File.
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("videoMetadata")]
-        public global::Google.Gemini.VideoFileMetadata? VideoMetadata { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("source")]
+        [global::System.Text.Json.Serialization.JsonConverter(typeof(global::Google.Gemini.JsonConverters.FileSourceJsonConverter))]
+        public global::Google.Gemini.FileSource? Source { get; set; }
+
+        /// <summary>
+        /// Output only. The timestamp of when the `File` will be deleted. Only set if the `File` is scheduled to expire.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("expirationTime")]
+        public string? ExpirationTime { get; set; }
+
+        /// <summary>
+        /// Output only. Size of the file in bytes.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("sizeBytes")]
+        public string? SizeBytes { get; set; }
 
         /// <summary>
         /// Output only. Processing state of the File.<br/>
@@ -98,10 +84,24 @@ namespace Google.Gemini
         public global::Google.Gemini.FileState? State { get; set; }
 
         /// <summary>
-        /// Optional. The human-readable display name for the `File`. The display name must be no more than 512 characters in length, including spaces. Example: "Welcome Image"
+        /// Output only. The download uri of the `File`.<br/>
+        /// Included only in responses
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("displayName")]
-        public string? DisplayName { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("downloadUri")]
+        public string? DownloadUri { get; set; }
+
+        /// <summary>
+        /// Metadata for a video `File`.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("videoMetadata")]
+        public global::Google.Gemini.VideoFileMetadata? VideoMetadata { get; set; }
+
+        /// <summary>
+        /// Output only. The uri of the `File`.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("uri")]
+        public string? Uri { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -112,90 +112,90 @@ namespace Google.Gemini
         /// <summary>
         /// Initializes a new instance of the <see cref="File" /> class.
         /// </summary>
-        /// <param name="mimeType">
-        /// Output only. MIME type of the file.<br/>
+        /// <param name="displayName">
+        /// Optional. The human-readable display name for the `File`. The display name must be no more than 512 characters in length, including spaces. Example: "Welcome Image"
+        /// </param>
+        /// <param name="error">
+        /// The `Status` type defines a logical error model that is suitable for different programming environments, including REST APIs and RPC APIs. It is used by [gRPC](https://github.com/grpc). Each `Status` message contains three pieces of data: error code, error message, and error details. You can find out more about this error model and how to work with it in the [API Design Guide](https://cloud.google.com/apis/design/errors).
+        /// </param>
+        /// <param name="sha256Hash">
+        /// Output only. SHA-256 hash of the uploaded bytes.<br/>
         /// Included only in responses
         /// </param>
         /// <param name="createTime">
         /// Output only. The timestamp of when the `File` was created.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="expirationTime">
-        /// Output only. The timestamp of when the `File` will be deleted. Only set if the `File` is scheduled to expire.<br/>
+        /// <param name="mimeType">
+        /// Output only. MIME type of the file.<br/>
+        /// Included only in responses
+        /// </param>
+        /// <param name="name">
+        /// Immutable. Identifier. The `File` resource name. The ID (name excluding the "files/" prefix) can contain up to 40 characters that are lowercase alphanumeric or dashes (-). The ID cannot start or end with a dash. If the name is empty on create, a unique name will be generated. Example: `files/123-456`
+        /// </param>
+        /// <param name="updateTime">
+        /// Output only. The timestamp of when the `File` was last updated.<br/>
         /// Included only in responses
         /// </param>
         /// <param name="source">
         /// Source of the File.
         /// </param>
+        /// <param name="expirationTime">
+        /// Output only. The timestamp of when the `File` will be deleted. Only set if the `File` is scheduled to expire.<br/>
+        /// Included only in responses
+        /// </param>
         /// <param name="sizeBytes">
         /// Output only. Size of the file in bytes.<br/>
+        /// Included only in responses
+        /// </param>
+        /// <param name="state">
+        /// Output only. Processing state of the File.<br/>
         /// Included only in responses
         /// </param>
         /// <param name="downloadUri">
         /// Output only. The download uri of the `File`.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="error">
-        /// The `Status` type defines a logical error model that is suitable for different programming environments, including REST APIs and RPC APIs. It is used by [gRPC](https://github.com/grpc). Each `Status` message contains three pieces of data: error code, error message, and error details. You can find out more about this error model and how to work with it in the [API Design Guide](https://cloud.google.com/apis/design/errors).
-        /// </param>
-        /// <param name="name">
-        /// Immutable. Identifier. The `File` resource name. The ID (name excluding the "files/" prefix) can contain up to 40 characters that are lowercase alphanumeric or dashes (-). The ID cannot start or end with a dash. If the name is empty on create, a unique name will be generated. Example: `files/123-456`
+        /// <param name="videoMetadata">
+        /// Metadata for a video `File`.
         /// </param>
         /// <param name="uri">
         /// Output only. The uri of the `File`.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="sha256Hash">
-        /// Output only. SHA-256 hash of the uploaded bytes.<br/>
-        /// Included only in responses
-        /// </param>
-        /// <param name="updateTime">
-        /// Output only. The timestamp of when the `File` was last updated.<br/>
-        /// Included only in responses
-        /// </param>
-        /// <param name="videoMetadata">
-        /// Metadata for a video `File`.
-        /// </param>
-        /// <param name="state">
-        /// Output only. Processing state of the File.<br/>
-        /// Included only in responses
-        /// </param>
-        /// <param name="displayName">
-        /// Optional. The human-readable display name for the `File`. The display name must be no more than 512 characters in length, including spaces. Example: "Welcome Image"
-        /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public File(
-            string? mimeType,
-            string? createTime,
-            string? expirationTime,
-            global::Google.Gemini.FileSource? source,
-            string? sizeBytes,
-            string? downloadUri,
+            string? displayName,
             global::Google.Gemini.Status? error,
-            string? name,
-            string? uri,
             byte[]? sha256Hash,
+            string? createTime,
+            string? mimeType,
+            string? name,
             string? updateTime,
-            global::Google.Gemini.VideoFileMetadata? videoMetadata,
+            global::Google.Gemini.FileSource? source,
+            string? expirationTime,
+            string? sizeBytes,
             global::Google.Gemini.FileState? state,
-            string? displayName)
+            string? downloadUri,
+            global::Google.Gemini.VideoFileMetadata? videoMetadata,
+            string? uri)
         {
-            this.MimeType = mimeType;
-            this.CreateTime = createTime;
-            this.ExpirationTime = expirationTime;
-            this.Source = source;
-            this.SizeBytes = sizeBytes;
-            this.DownloadUri = downloadUri;
-            this.Error = error;
-            this.Name = name;
-            this.Uri = uri;
-            this.Sha256Hash = sha256Hash;
-            this.UpdateTime = updateTime;
-            this.VideoMetadata = videoMetadata;
-            this.State = state;
             this.DisplayName = displayName;
+            this.Error = error;
+            this.Sha256Hash = sha256Hash;
+            this.CreateTime = createTime;
+            this.MimeType = mimeType;
+            this.Name = name;
+            this.UpdateTime = updateTime;
+            this.Source = source;
+            this.ExpirationTime = expirationTime;
+            this.SizeBytes = sizeBytes;
+            this.State = state;
+            this.DownloadUri = downloadUri;
+            this.VideoMetadata = videoMetadata;
+            this.Uri = uri;
         }
 
         /// <summary>
--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.UsageMetadata.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.UsageMetadata.g.cs
@@ -9,17 +9,11 @@ namespace Google.Gemini
     public sealed partial class UsageMetadata
     {
         /// <summary>
-        /// Output only. List of modalities that were returned in the response.<br/>
+        /// Output only. List of modalities that were processed in the request input.<br/>
         /// Included only in responses
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("candidatesTokensDetails")]
-        public global::System.Collections.Generic.IList<global::Google.Gemini.ModalityTokenCount>? CandidatesTokensDetails { get; set; }
-
-        /// <summary>
-        /// Total number of tokens across all the generated response candidates.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("candidatesTokenCount")]
-        public int? CandidatesTokenCount { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("promptTokensDetails")]
+        public global::System.Collections.Generic.IList<global::Google.Gemini.ModalityTokenCount>? PromptTokensDetails { get; set; }
 
         /// <summary>
         /// Total token count for the generation request (prompt + thoughts + response candidates).
@@ -28,11 +22,10 @@ namespace Google.Gemini
         public int? TotalTokenCount { get; set; }
 
         /// <summary>
-        /// Output only. List of modalities of the cached content in the request input.<br/>
-        /// Included only in responses
+        /// Number of tokens in the cached part of the prompt (the cached content)
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("cacheTokensDetails")]
-        public global::System.Collections.Generic.IList<global::Google.Gemini.ModalityTokenCount>? CacheTokensDetails { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("cachedContentTokenCount")]
+        public int? CachedContentTokenCount { get; set; }
 
         /// <summary>
         /// Output only. List of modalities that were processed for tool-use request inputs.<br/>
@@ -40,6 +33,27 @@ namespace Google.Gemini
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("toolUsePromptTokensDetails")]
         public global::System.Collections.Generic.IList<global::Google.Gemini.ModalityTokenCount>? ToolUsePromptTokensDetails { get; set; }
+
+        /// <summary>
+        /// Output only. Number of tokens present in tool-use prompt(s).<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("toolUsePromptTokenCount")]
+        public int? ToolUsePromptTokenCount { get; set; }
+
+        /// <summary>
+        /// Output only. List of modalities that were returned in the response.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("candidatesTokensDetails")]
+        public global::System.Collections.Generic.IList<global::Google.Gemini.ModalityTokenCount>? CandidatesTokensDetails { get; set; }
+
+        /// <summary>
+        /// Output only. Number of tokens of thoughts for thinking models.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("thoughtsTokenCount")]
+        public int? ThoughtsTokenCount { get; set; }
 
         /// <summary>
         /// Output only. Service tier of the request.<br/>
@@ -50,37 +64,23 @@ namespace Google.Gemini
         public global::Google.Gemini.UsageMetadataServiceTier? ServiceTier { get; set; }
 
         /// <summary>
-        /// Output only. Number of tokens present in tool-use prompt(s).<br/>
+        /// Output only. List of modalities of the cached content in the request input.<br/>
         /// Included only in responses
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("toolUsePromptTokenCount")]
-        public int? ToolUsePromptTokenCount { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("cacheTokensDetails")]
+        public global::System.Collections.Generic.IList<global::Google.Gemini.ModalityTokenCount>? CacheTokensDetails { get; set; }
+
+        /// <summary>
+        /// Total number of tokens across all the generated response candidates.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("candidatesTokenCount")]
+        public int? CandidatesTokenCount { get; set; }
 
         /// <summary>
         /// Number of tokens in the prompt. When `cached_content` is set, this is still the total effective prompt size meaning this includes the number of tokens in the cached content.
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("promptTokenCount")]
         public int? PromptTokenCount { get; set; }
-
-        /// <summary>
-        /// Number of tokens in the cached part of the prompt (the cached content)
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("cachedContentTokenCount")]
-        public int? CachedContentTokenCount { get; set; }
-
-        /// <summary>
-        /// Output only. Number of tokens of thoughts for thinking models.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("thoughtsTokenCount")]
-        public int? ThoughtsTokenCount { get; set; }
-
-        /// <summary>
-        /// Output only. List of modalities that were processed in the request input.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("promptTokensDetails")]
-        public global::System.Collections.Generic.IList<global::Google.Gemini.ModalityTokenCount>? PromptTokensDetails { get; set; }
 
         /// <summary>
         /// Output only. Number of tokens in the response. Used by Gemini 3.1+ Live API models instead of candidatesTokenCount.<br/>
@@ -105,45 +105,45 @@ namespace Google.Gemini
         /// <summary>
         /// Initializes a new instance of the <see cref="UsageMetadata" /> class.
         /// </summary>
-        /// <param name="candidatesTokensDetails">
-        /// Output only. List of modalities that were returned in the response.<br/>
+        /// <param name="promptTokensDetails">
+        /// Output only. List of modalities that were processed in the request input.<br/>
         /// Included only in responses
-        /// </param>
-        /// <param name="candidatesTokenCount">
-        /// Total number of tokens across all the generated response candidates.
         /// </param>
         /// <param name="totalTokenCount">
         /// Total token count for the generation request (prompt + thoughts + response candidates).
         /// </param>
-        /// <param name="cacheTokensDetails">
-        /// Output only. List of modalities of the cached content in the request input.<br/>
-        /// Included only in responses
+        /// <param name="cachedContentTokenCount">
+        /// Number of tokens in the cached part of the prompt (the cached content)
         /// </param>
         /// <param name="toolUsePromptTokensDetails">
         /// Output only. List of modalities that were processed for tool-use request inputs.<br/>
-        /// Included only in responses
-        /// </param>
-        /// <param name="serviceTier">
-        /// Output only. Service tier of the request.<br/>
         /// Included only in responses
         /// </param>
         /// <param name="toolUsePromptTokenCount">
         /// Output only. Number of tokens present in tool-use prompt(s).<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="promptTokenCount">
-        /// Number of tokens in the prompt. When `cached_content` is set, this is still the total effective prompt size meaning this includes the number of tokens in the cached content.
-        /// </param>
-        /// <param name="cachedContentTokenCount">
-        /// Number of tokens in the cached part of the prompt (the cached content)
+        /// <param name="candidatesTokensDetails">
+        /// Output only. List of modalities that were returned in the response.<br/>
+        /// Included only in responses
         /// </param>
         /// <param name="thoughtsTokenCount">
         /// Output only. Number of tokens of thoughts for thinking models.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="promptTokensDetails">
-        /// Output only. List of modalities that were processed in the request input.<br/>
+        /// <param name="serviceTier">
+        /// Output only. Service tier of the request.<br/>
         /// Included only in responses
+        /// </param>
+        /// <param name="cacheTokensDetails">
+        /// Output only. List of modalities of the cached content in the request input.<br/>
+        /// Included only in responses
+        /// </param>
+        /// <param name="candidatesTokenCount">
+        /// Total number of tokens across all the generated response candidates.
+        /// </param>
+        /// <param name="promptTokenCount">
+        /// Number of tokens in the prompt. When `cached_content` is set, this is still the total effective prompt size meaning this includes the number of tokens in the cached content.
         /// </param>
         /// <param name="responseTokenCount">
         /// Output only. Number of tokens in the response. Used by Gemini 3.1+ Live API models instead of candidatesTokenCount.<br/>
@@ -157,31 +157,31 @@ namespace Google.Gemini
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public UsageMetadata(
-            global::System.Collections.Generic.IList<global::Google.Gemini.ModalityTokenCount>? candidatesTokensDetails,
-            int? candidatesTokenCount,
-            int? totalTokenCount,
-            global::System.Collections.Generic.IList<global::Google.Gemini.ModalityTokenCount>? cacheTokensDetails,
-            global::System.Collections.Generic.IList<global::Google.Gemini.ModalityTokenCount>? toolUsePromptTokensDetails,
-            global::Google.Gemini.UsageMetadataServiceTier? serviceTier,
-            int? toolUsePromptTokenCount,
-            int? promptTokenCount,
-            int? cachedContentTokenCount,
-            int? thoughtsTokenCount,
             global::System.Collections.Generic.IList<global::Google.Gemini.ModalityTokenCount>? promptTokensDetails,
+            int? totalTokenCount,
+            int? cachedContentTokenCount,
+            global::System.Collections.Generic.IList<global::Google.Gemini.ModalityTokenCount>? toolUsePromptTokensDetails,
+            int? toolUsePromptTokenCount,
+            global::System.Collections.Generic.IList<global::Google.Gemini.ModalityTokenCount>? candidatesTokensDetails,
+            int? thoughtsTokenCount,
+            global::Google.Gemini.UsageMetadataServiceTier? serviceTier,
+            global::System.Collections.Generic.IList<global::Google.Gemini.ModalityTokenCount>? cacheTokensDetails,
+            int? candidatesTokenCount,
+            int? promptTokenCount,
             int? responseTokenCount,
             global::System.Collections.Generic.IList<global::Google.Gemini.ModalityTokenCount>? responseTokensDetails)
         {
-            this.CandidatesTokensDetails = candidatesTokensDetails;
-            this.CandidatesTokenCount = candidatesTokenCount;
-            this.TotalTokenCount = totalTokenCount;
-            this.CacheTokensDetails = cacheTokensDetails;
-            this.ToolUsePromptTokensDetails = toolUsePromptTokensDetails;
-            this.ServiceTier = serviceTier;
-            this.ToolUsePromptTokenCount = toolUsePromptTokenCount;
-            this.PromptTokenCount = promptTokenCount;
-            this.CachedContentTokenCount = cachedContentTokenCount;
-            this.ThoughtsTokenCount = thoughtsTokenCount;
             this.PromptTokensDetails = promptTokensDetails;
+            this.TotalTokenCount = totalTokenCount;
+            this.CachedContentTokenCount = cachedContentTokenCount;
+            this.ToolUsePromptTokensDetails = toolUsePromptTokensDetails;
+            this.ToolUsePromptTokenCount = toolUsePromptTokenCount;
+            this.CandidatesTokensDetails = candidatesTokensDetails;
+            this.ThoughtsTokenCount = thoughtsTokenCount;
+            this.ServiceTier = serviceTier;
+            this.CacheTokensDetails = cacheTokensDetails;
+            this.CandidatesTokenCount = candidatesTokenCount;
+            this.PromptTokenCount = promptTokenCount;
             this.ResponseTokenCount = responseTokenCount;
             this.ResponseTokensDetails = responseTokensDetails;
         }
