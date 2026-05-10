5d1757451dc3a461803e9bd26d069229290a8656
Konstantin S. <havendv@gmail.com>
2026-05-10 23:01:23 +0400

    feat: Updated OpenAPI spec (#394)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.BatchStats.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.BatchStats.g.cs
@@ -9,18 +9,18 @@ namespace Google.Gemini
     public sealed partial class BatchStats
     {
         /// <summary>
-        /// Output only. The number of requests in the batch.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("requestCount")]
-        public string? RequestCount { get; set; }
-
-        /// <summary>
         /// Output only. The number of requests that failed to be processed.<br/>
         /// Included only in responses
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("failedRequestCount")]
         public string? FailedRequestCount { get; set; }
+
+        /// <summary>
+        /// Output only. The number of requests in the batch.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("requestCount")]
+        public string? RequestCount { get; set; }
 
         /// <summary>
         /// Output only. The number of requests that were successfully processed.<br/>
@@ -45,12 +45,12 @@ namespace Google.Gemini
         /// <summary>
         /// Initializes a new instance of the <see cref="BatchStats" /> class.
         /// </summary>
-        /// <param name="requestCount">
-        /// Output only. The number of requests in the batch.<br/>
-        /// Included only in responses
-        /// </param>
         /// <param name="failedRequestCount">
         /// Output only. The number of requests that failed to be processed.<br/>
+        /// Included only in responses
+        /// </param>
+        /// <param name="requestCount">
+        /// Output only. The number of requests in the batch.<br/>
         /// Included only in responses
         /// </param>
         /// <param name="successfulRequestCount">
@@ -65,13 +65,13 @@ namespace Google.Gemini
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public BatchStats(
-            string? requestCount,
             string? failedRequestCount,
+            string? requestCount,
             string? successfulRequestCount,
             string? pendingRequestCount)
         {
-            this.RequestCount = requestCount;
             this.FailedRequestCount = failedRequestCount;
+            this.RequestCount = requestCount;
             this.SuccessfulRequestCount = successfulRequestCount;
             this.PendingRequestCount = pendingRequestCount;
         }
