01d4da223aa748eecd290d84bba41ea091d8652e
Konstantin S. <havendv@gmail.com>
2026-05-13 06:55:15 +0400

    feat: Updated OpenAPI spec (#399)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.FunctionResponseResponse.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.FunctionResponseResponse.g.cs
@@ -4,7 +4,7 @@
 namespace Google.Gemini
 {
     /// <summary>
-    /// Required. The function response in JSON object format. Callers can use any keys of their choice that fit the function's syntax to return the function output, e.g. "output", "result", etc. In particular, if the function call failed to execute, the response can have an "error" key to return error details to the model.
+    /// Required. The function response in JSON object format. Callers can use any keys of their choice that fit the function's syntax to return the function output, e.g. "output", "result", etc. In particular, if the function call failed to execute, the response can have an "error" key to return error details to the model. Multimedia can be included by using a subobject containing a single "$ref" key whose value is the `inline_data.display_name` of a `FunctionResponsePart` holding the multimedia. See https://ai.google.dev/gemini-api/docs/function-calling#multimodal.
     /// </summary>
     public sealed partial class FunctionResponseResponse
     {
