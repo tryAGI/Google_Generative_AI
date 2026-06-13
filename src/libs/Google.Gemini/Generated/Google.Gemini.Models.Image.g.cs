6cc64b75111772e37522ddccb87815a9fab5f026
Konstantin S. <havendv@gmail.com>
2026-06-13 07:34:51 +0400

    feat: Updated OpenAPI spec (#430)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.Image.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.Image.g.cs
@@ -9,6 +9,12 @@ namespace Google.Gemini
     public sealed partial class Image
     {
         /// <summary>
+        /// The image asset URL.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("imageUri")]
+        public string? ImageUri { get; set; }
+
+        /// <summary>
         /// The title of the web page that the image is from.
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("title")]
@@ -19,12 +25,6 @@ namespace Google.Gemini
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("sourceUri")]
         public string? SourceUri { get; set; }
-
-        /// <summary>
-        /// The image asset URL.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("imageUri")]
-        public string? ImageUri { get; set; }
 
         /// <summary>
         /// The root domain of the web page that the image is from, e.g. "example.com".
@@ -41,14 +41,14 @@ namespace Google.Gemini
         /// <summary>
         /// Initializes a new instance of the <see cref="Image" /> class.
         /// </summary>
+        /// <param name="imageUri">
+        /// The image asset URL.
+        /// </param>
         /// <param name="title">
         /// The title of the web page that the image is from.
         /// </param>
         /// <param name="sourceUri">
         /// The web page URI for attribution.
-        /// </param>
-        /// <param name="imageUri">
-        /// The image asset URL.
         /// </param>
         /// <param name="domain">
         /// The root domain of the web page that the image is from, e.g. "example.com".
@@ -57,14 +57,14 @@ namespace Google.Gemini
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public Image(
+            string? imageUri,
             string? title,
             string? sourceUri,
-            string? imageUri,
             string? domain)
         {
+            this.ImageUri = imageUri;
             this.Title = title;
             this.SourceUri = sourceUri;
-            this.ImageUri = imageUri;
             this.Domain = domain;
         }
 
