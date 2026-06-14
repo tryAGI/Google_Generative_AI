7f090fb1e5d9846d9fbb65b4272f4ecbd9ed12b1
Konstantin S. <havendv@gmail.com>
2026-06-14 07:55:58 +0400

    feat: Updated OpenAPI spec (#437)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.GenerateContentResponse.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.GenerateContentResponse.g.cs
@@ -9,18 +9,6 @@ namespace Google.Gemini
     public sealed partial class GenerateContentResponse
     {
         /// <summary>
-        /// The status of the underlying model. This is used to indicate the stage of the underlying model and the retirement time if applicable.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("modelStatus")]
-        public global::Google.Gemini.ModelStatus? ModelStatus { get; set; }
-
-        /// <summary>
-        /// A set of the feedback metadata the prompt specified in `GenerateContentRequest.content`.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("promptFeedback")]
-        public global::Google.Gemini.PromptFeedback? PromptFeedback { get; set; }
-
-        /// <summary>
         /// Candidate responses from the model.
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("candidates")]
@@ -34,11 +22,23 @@ namespace Google.Gemini
         public string? ModelVersion { get; set; }
 
         /// <summary>
+        /// A set of the feedback metadata the prompt specified in `GenerateContentRequest.content`.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("promptFeedback")]
+        public global::Google.Gemini.PromptFeedback? PromptFeedback { get; set; }
+
+        /// <summary>
         /// Output only. response_id is used to identify each response.<br/>
         /// Included only in responses
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("responseId")]
         public string? ResponseId { get; set; }
+
+        /// <summary>
+        /// The status of the underlying model. This is used to indicate the stage of the underlying model and the retirement time if applicable.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("modelStatus")]
+        public global::Google.Gemini.ModelStatus? ModelStatus { get; set; }
 
         /// <summary>
         /// Metadata on the generation request's token usage.
@@ -55,12 +55,6 @@ namespace Google.Gemini
         /// <summary>
         /// Initializes a new instance of the <see cref="GenerateContentResponse" /> class.
         /// </summary>
-        /// <param name="modelStatus">
-        /// The status of the underlying model. This is used to indicate the stage of the underlying model and the retirement time if applicable.
-        /// </param>
-        /// <param name="promptFeedback">
-        /// A set of the feedback metadata the prompt specified in `GenerateContentRequest.content`.
-        /// </param>
         /// <param name="candidates">
         /// Candidate responses from the model.
         /// </param>
@@ -68,9 +62,15 @@ namespace Google.Gemini
         /// Output only. The model version used to generate the response.<br/>
         /// Included only in responses
         /// </param>
+        /// <param name="promptFeedback">
+        /// A set of the feedback metadata the prompt specified in `GenerateContentRequest.content`.
+        /// </param>
         /// <param name="responseId">
         /// Output only. response_id is used to identify each response.<br/>
         /// Included only in responses
+        /// </param>
+        /// <param name="modelStatus">
+        /// The status of the underlying model. This is used to indicate the stage of the underlying model and the retirement time if applicable.
         /// </param>
         /// <param name="usageMetadata">
         /// Metadata on the generation request's token usage.
@@ -79,18 +79,18 @@ namespace Google.Gemini
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public GenerateContentResponse(
-            global::Google.Gemini.ModelStatus? modelStatus,
-            global::Google.Gemini.PromptFeedback? promptFeedback,
             global::System.Collections.Generic.IList<global::Google.Gemini.Candidate>? candidates,
             string? modelVersion,
+            global::Google.Gemini.PromptFeedback? promptFeedback,
             string? responseId,
+            global::Google.Gemini.ModelStatus? modelStatus,
             global::Google.Gemini.UsageMetadata? usageMetadata)
         {
-            this.ModelStatus = modelStatus;
-            this.PromptFeedback = promptFeedback;
             this.Candidates = candidates;
             this.ModelVersion = modelVersion;
+            this.PromptFeedback = promptFeedback;
             this.ResponseId = responseId;
+            this.ModelStatus = modelStatus;
             this.UsageMetadata = usageMetadata;
         }
 
--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.TuningSnapshot.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.TuningSnapshot.g.cs
@@ -9,6 +9,13 @@ namespace Google.Gemini
     public sealed partial class TuningSnapshot
     {
         /// <summary>
+        /// Output only. The tuning step.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("step")]
+        public int? Step { get; set; }
+
+        /// <summary>
         /// Output only. The epoch this step was part of.<br/>
         /// Included only in responses
         /// </summary>
@@ -30,13 +37,6 @@ namespace Google.Gemini
         public string? ComputeTime { get; set; }
 
         /// <summary>
-        /// Output only. The tuning step.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("step")]
-        public int? Step { get; set; }
-
-        /// <summary>
         /// Additional properties that are not explicitly defined in the schema
         /// </summary>
         [global::System.Text.Json.Serialization.JsonExtensionData]
@@ -45,6 +45,10 @@ namespace Google.Gemini
         /// <summary>
         /// Initializes a new instance of the <see cref="TuningSnapshot" /> class.
         /// </summary>
+        /// <param name="step">
+        /// Output only. The tuning step.<br/>
+        /// Included only in responses
+        /// </param>
         /// <param name="epoch">
         /// Output only. The epoch this step was part of.<br/>
         /// Included only in responses
@@ -57,23 +61,19 @@ namespace Google.Gemini
         /// Output only. The timestamp when this metric was computed.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="step">
-        /// Output only. The tuning step.<br/>
-        /// Included only in responses
-        /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public TuningSnapshot(
+            int? step,
             int? epoch,
             float? meanLoss,
-            string? computeTime,
-            int? step)
+            string? computeTime)
         {
+            this.Step = step;
             this.Epoch = epoch;
             this.MeanLoss = meanLoss;
             this.ComputeTime = computeTime;
-            this.Step = step;
         }
 
         /// <summary>
