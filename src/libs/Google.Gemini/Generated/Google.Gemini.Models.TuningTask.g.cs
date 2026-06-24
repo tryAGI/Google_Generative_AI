589cda15117926654104a1edbe69bfe5715d6685
Konstantin S. <havendv@gmail.com>
2026-06-24 18:26:03 +0400

    feat: Updated OpenAPI spec (#495)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.TuningTask.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.TuningTask.g.cs
@@ -9,12 +9,6 @@ namespace Google.Gemini
     public sealed partial class TuningTask
     {
         /// <summary>
-        /// Hyperparameters controlling the tuning process. Read more at https://ai.google.dev/docs/model_tuning_guidance
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("hyperparameters")]
-        public global::Google.Gemini.Hyperparameters? Hyperparameters { get; set; }
-
-        /// <summary>
         /// Output only. Metrics collected during tuning.<br/>
         /// Included only in responses
         /// </summary>
@@ -22,10 +16,10 @@ namespace Google.Gemini
         public global::System.Collections.Generic.IList<global::Google.Gemini.TuningSnapshot>? Snapshots { get; set; }
 
         /// <summary>
-        /// Dataset for training or validation.
+        /// Hyperparameters controlling the tuning process. Read more at https://ai.google.dev/docs/model_tuning_guidance
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("trainingData")]
-        public global::Google.Gemini.Dataset? TrainingData { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("hyperparameters")]
+        public global::Google.Gemini.Hyperparameters? Hyperparameters { get; set; }
 
         /// <summary>
         /// Output only. The timestamp when tuning this model completed.<br/>
@@ -33,6 +27,12 @@ namespace Google.Gemini
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("completeTime")]
         public string? CompleteTime { get; set; }
+
+        /// <summary>
+        /// Dataset for training or validation.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("trainingData")]
+        public global::Google.Gemini.Dataset? TrainingData { get; set; }
 
         /// <summary>
         /// Output only. The timestamp when tuning this model started.<br/>
@@ -50,19 +50,19 @@ namespace Google.Gemini
         /// <summary>
         /// Initializes a new instance of the <see cref="TuningTask" /> class.
         /// </summary>
-        /// <param name="hyperparameters">
-        /// Hyperparameters controlling the tuning process. Read more at https://ai.google.dev/docs/model_tuning_guidance
-        /// </param>
         /// <param name="snapshots">
         /// Output only. Metrics collected during tuning.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="trainingData">
-        /// Dataset for training or validation.
+        /// <param name="hyperparameters">
+        /// Hyperparameters controlling the tuning process. Read more at https://ai.google.dev/docs/model_tuning_guidance
         /// </param>
         /// <param name="completeTime">
         /// Output only. The timestamp when tuning this model completed.<br/>
         /// Included only in responses
+        /// </param>
+        /// <param name="trainingData">
+        /// Dataset for training or validation.
         /// </param>
         /// <param name="startTime">
         /// Output only. The timestamp when tuning this model started.<br/>
@@ -72,16 +72,16 @@ namespace Google.Gemini
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public TuningTask(
-            global::Google.Gemini.Hyperparameters? hyperparameters,
             global::System.Collections.Generic.IList<global::Google.Gemini.TuningSnapshot>? snapshots,
-            global::Google.Gemini.Dataset? trainingData,
+            global::Google.Gemini.Hyperparameters? hyperparameters,
             string? completeTime,
+            global::Google.Gemini.Dataset? trainingData,
             string? startTime)
         {
-            this.Hyperparameters = hyperparameters;
             this.Snapshots = snapshots;
-            this.TrainingData = trainingData;
+            this.Hyperparameters = hyperparameters;
             this.CompleteTime = completeTime;
+            this.TrainingData = trainingData;
             this.StartTime = startTime;
         }
 
