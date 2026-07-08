2d1f1641c4074e1356a41f312f57c35582adc559
Konstantin S. <havendv@gmail.com>
2026-07-09 02:03:54 +0400

    feat: Updated OpenAPI spec (#589)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.CachedContent.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.CachedContent.g.cs
@@ -9,35 +9,10 @@ namespace Google.Gemini
     public sealed partial class CachedContent
     {
         /// <summary>
-        /// Timestamp in UTC of when this resource is considered expired. This is *always* provided on output, regardless of what was sent on input.
+        /// The base structured datatype containing multi-part content of a message. A `Content` includes a `role` field designating the producer of the `Content` and a `parts` field containing multi-part data that contains the content of the message turn.
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("expireTime")]
-        public string? ExpireTime { get; set; }
-
-        /// <summary>
-        /// Optional. Input only. Immutable. The content to cache.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("contents")]
-        public global::System.Collections.Generic.IList<global::Google.Gemini.Content>? Contents { get; set; }
-
-        /// <summary>
-        /// The Tool configuration containing parameters for specifying `Tool` use in the request.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("toolConfig")]
-        public global::Google.Gemini.ToolConfig? ToolConfig { get; set; }
-
-        /// <summary>
-        /// Output only. Creation time of the cache entry.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("createTime")]
-        public string? CreateTime { get; set; }
-
-        /// <summary>
-        /// Metadata on the usage of the cached content.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("usageMetadata")]
-        public global::Google.Gemini.CachedContentUsageMetadata? UsageMetadata { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("systemInstruction")]
+        public global::Google.Gemini.Content? SystemInstruction { get; set; }
 
         /// <summary>
         /// Input only. New TTL for this resource, input only.
@@ -46,29 +21,10 @@ namespace Google.Gemini
         public string? Ttl { get; set; }
 
         /// <summary>
-        /// Optional. Immutable. The user-generated meaningful display name of the cached content. Maximum 128 Unicode characters.
+        /// Optional. Input only. Immutable. The content to cache.
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("displayName")]
-        public string? DisplayName { get; set; }
-
-        /// <summary>
-        /// Optional. Input only. Immutable. A list of `Tools` the model may use to generate the next response
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("tools")]
-        public global::System.Collections.Generic.IList<global::Google.Gemini.Tool>? Tools { get; set; }
-
-        /// <summary>
-        /// Output only. Identifier. The resource name referring to the cached content. Format: `cachedContents/{id}`<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("name")]
-        public string? Name { get; set; }
-
-        /// <summary>
-        /// The base structured datatype containing multi-part content of a message. A `Content` includes a `role` field designating the producer of the `Content` and a `parts` field containing multi-part data that contains the content of the message turn.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("systemInstruction")]
-        public global::Google.Gemini.Content? SystemInstruction { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("contents")]
+        public global::System.Collections.Generic.IList<global::Google.Gemini.Content>? Contents { get; set; }
 
         /// <summary>
         /// Output only. When the cache entry was last updated in UTC time.<br/>
@@ -78,10 +34,54 @@ namespace Google.Gemini
         public string? UpdateTime { get; set; }
 
         /// <summary>
+        /// Optional. Input only. Immutable. A list of `Tools` the model may use to generate the next response
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("tools")]
+        public global::System.Collections.Generic.IList<global::Google.Gemini.Tool>? Tools { get; set; }
+
+        /// <summary>
+        /// Optional. Immutable. The user-generated meaningful display name of the cached content. Maximum 128 Unicode characters.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("displayName")]
+        public string? DisplayName { get; set; }
+
+        /// <summary>
+        /// Metadata on the usage of the cached content.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("usageMetadata")]
+        public global::Google.Gemini.CachedContentUsageMetadata? UsageMetadata { get; set; }
+
+        /// <summary>
+        /// Output only. Identifier. The resource name referring to the cached content. Format: `cachedContents/{id}`<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        /// <summary>
         /// Required. Immutable. The name of the `Model` to use for cached content Format: `models/{model}`
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("model")]
         public string? Model { get; set; }
+
+        /// <summary>
+        /// Output only. Creation time of the cache entry.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("createTime")]
+        public string? CreateTime { get; set; }
+
+        /// <summary>
+        /// Timestamp in UTC of when this resource is considered expired. This is *always* provided on output, regardless of what was sent on input.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("expireTime")]
+        public string? ExpireTime { get; set; }
+
+        /// <summary>
+        /// The Tool configuration containing parameters for specifying `Tool` use in the request.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("toolConfig")]
+        public global::Google.Gemini.ToolConfig? ToolConfig { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -92,74 +92,74 @@ namespace Google.Gemini
         /// <summary>
         /// Initializes a new instance of the <see cref="CachedContent" /> class.
         /// </summary>
-        /// <param name="expireTime">
-        /// Timestamp in UTC of when this resource is considered expired. This is *always* provided on output, regardless of what was sent on input.
-        /// </param>
-        /// <param name="contents">
-        /// Optional. Input only. Immutable. The content to cache.
-        /// </param>
-        /// <param name="toolConfig">
-        /// The Tool configuration containing parameters for specifying `Tool` use in the request.
-        /// </param>
-        /// <param name="createTime">
-        /// Output only. Creation time of the cache entry.<br/>
-        /// Included only in responses
-        /// </param>
-        /// <param name="usageMetadata">
-        /// Metadata on the usage of the cached content.
+        /// <param name="systemInstruction">
+        /// The base structured datatype containing multi-part content of a message. A `Content` includes a `role` field designating the producer of the `Content` and a `parts` field containing multi-part data that contains the content of the message turn.
         /// </param>
         /// <param name="ttl">
         /// Input only. New TTL for this resource, input only.
         /// </param>
-        /// <param name="displayName">
-        /// Optional. Immutable. The user-generated meaningful display name of the cached content. Maximum 128 Unicode characters.
-        /// </param>
-        /// <param name="tools">
-        /// Optional. Input only. Immutable. A list of `Tools` the model may use to generate the next response
-        /// </param>
-        /// <param name="name">
-        /// Output only. Identifier. The resource name referring to the cached content. Format: `cachedContents/{id}`<br/>
-        /// Included only in responses
-        /// </param>
-        /// <param name="systemInstruction">
-        /// The base structured datatype containing multi-part content of a message. A `Content` includes a `role` field designating the producer of the `Content` and a `parts` field containing multi-part data that contains the content of the message turn.
+        /// <param name="contents">
+        /// Optional. Input only. Immutable. The content to cache.
         /// </param>
         /// <param name="updateTime">
         /// Output only. When the cache entry was last updated in UTC time.<br/>
         /// Included only in responses
         /// </param>
+        /// <param name="tools">
+        /// Optional. Input only. Immutable. A list of `Tools` the model may use to generate the next response
+        /// </param>
+        /// <param name="displayName">
+        /// Optional. Immutable. The user-generated meaningful display name of the cached content. Maximum 128 Unicode characters.
+        /// </param>
+        /// <param name="usageMetadata">
+        /// Metadata on the usage of the cached content.
+        /// </param>
+        /// <param name="name">
+        /// Output only. Identifier. The resource name referring to the cached content. Format: `cachedContents/{id}`<br/>
+        /// Included only in responses
+        /// </param>
         /// <param name="model">
         /// Required. Immutable. The name of the `Model` to use for cached content Format: `models/{model}`
+        /// </param>
+        /// <param name="createTime">
+        /// Output only. Creation time of the cache entry.<br/>
+        /// Included only in responses
+        /// </param>
+        /// <param name="expireTime">
+        /// Timestamp in UTC of when this resource is considered expired. This is *always* provided on output, regardless of what was sent on input.
+        /// </param>
+        /// <param name="toolConfig">
+        /// The Tool configuration containing parameters for specifying `Tool` use in the request.
         /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public CachedContent(
-            string? expireTime,
-            global::System.Collections.Generic.IList<global::Google.Gemini.Content>? contents,
-            global::Google.Gemini.ToolConfig? toolConfig,
-            string? createTime,
-            global::Google.Gemini.CachedContentUsageMetadata? usageMetadata,
-            string? ttl,
-            string? displayName,
-            global::System.Collections.Generic.IList<global::Google.Gemini.Tool>? tools,
-            string? name,
             global::Google.Gemini.Content? systemInstruction,
+            string? ttl,
+            global::System.Collections.Generic.IList<global::Google.Gemini.Content>? contents,
             string? updateTime,
-            string? model)
+            global::System.Collections.Generic.IList<global::Google.Gemini.Tool>? tools,
+            string? displayName,
+            global::Google.Gemini.CachedContentUsageMetadata? usageMetadata,
+            string? name,
+            string? model,
+            string? createTime,
+            string? expireTime,
+            global::Google.Gemini.ToolConfig? toolConfig)
         {
-            this.ExpireTime = expireTime;
-            this.Contents = contents;
-            this.ToolConfig = toolConfig;
-            this.CreateTime = createTime;
-            this.UsageMetadata = usageMetadata;
-            this.Ttl = ttl;
-            this.DisplayName = displayName;
-            this.Tools = tools;
-            this.Name = name;
             this.SystemInstruction = systemInstruction;
+            this.Ttl = ttl;
+            this.Contents = contents;
             this.UpdateTime = updateTime;
+            this.Tools = tools;
+            this.DisplayName = displayName;
+            this.UsageMetadata = usageMetadata;
+            this.Name = name;
             this.Model = model;
+            this.CreateTime = createTime;
+            this.ExpireTime = expireTime;
+            this.ToolConfig = toolConfig;
         }
 
         /// <summary>
--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.File.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.File.g.cs
@@ -9,11 +9,18 @@ namespace Google.Gemini
     public sealed partial class File
     {
         /// <summary>
-        /// Output only. SHA-256 hash of the uploaded bytes.<br/>
+        /// Source of the File.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("source")]
+        [global::System.Text.Json.Serialization.JsonConverter(typeof(global::Google.Gemini.JsonConverters.FileSourceJsonConverter))]
+        public global::Google.Gemini.FileSource? Source { get; set; }
+
+        /// <summary>
+        /// Output only. The timestamp of when the `File` was last updated.<br/>
         /// Included only in responses
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("sha256Hash")]
-        public byte[]? Sha256Hash { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("updateTime")]
+        public string? UpdateTime { get; set; }
 
         /// <summary>
         /// Output only. Processing state of the File.<br/>
@@ -24,24 +31,17 @@ namespace Google.Gemini
         public global::Google.Gemini.FileState? State { get; set; }
 
         /// <summary>
-        /// Output only. The download uri of the `File`.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("downloadUri")]
-        public string? DownloadUri { get; set; }
-
-        /// <summary>
-        /// Output only. The timestamp of when the `File` was last updated.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("updateTime")]
-        public string? UpdateTime { get; set; }
-
-        /// <summary>
         /// The `Status` type defines a logical error model that is suitable for different programming environments, including REST APIs and RPC APIs. It is used by [gRPC](https://github.com/grpc). Each `Status` message contains three pieces of data: error code, error message, and error details. You can find out more about this error model and how to work with it in the [API Design Guide](https://cloud.google.com/apis/design/errors).
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("error")]
         public global::Google.Gemini.Status? Error { get; set; }
+
+        /// <summary>
+        /// Output only. SHA-256 hash of the uploaded bytes.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("sha256Hash")]
+        public byte[]? Sha256Hash { get; set; }
 
         /// <summary>
         /// Immutable. Identifier. The `File` resource name. The ID (name excluding the "files/" prefix) can contain up to 40 characters that are lowercase alphanumeric or dashes (-). The ID cannot start or end with a dash. If the name is empty on create, a unique name will be generated. Example: `files/123-456`
@@ -50,11 +50,30 @@ namespace Google.Gemini
         public string? Name { get; set; }
 
         /// <summary>
-        /// Output only. Size of the file in bytes.<br/>
+        /// Metadata for a video `File`.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("videoMetadata")]
+        public global::Google.Gemini.VideoFileMetadata? VideoMetadata { get; set; }
+
+        /// <summary>
+        /// Optional. The human-readable display name for the `File`. The display name must be no more than 512 characters in length, including spaces. Example: "Welcome Image"
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("displayName")]
+        public string? DisplayName { get; set; }
+
+        /// <summary>
+        /// Output only. The uri of the `File`.<br/>
         /// Included only in responses
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("sizeBytes")]
-        public string? SizeBytes { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("uri")]
+        public string? Uri { get; set; }
+
+        /// <summary>
+        /// Output only. The download uri of the `File`.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("downloadUri")]
+        public string? DownloadUri { get; set; }
 
         /// <summary>
         /// Output only. The timestamp of when the `File` will be deleted. Only set if the `File` is scheduled to expire.<br/>
@@ -64,11 +83,11 @@ namespace Google.Gemini
         public string? ExpirationTime { get; set; }
 
         /// <summary>
-        /// Output only. The uri of the `File`.<br/>
+        /// Output only. Size of the file in bytes.<br/>
         /// Included only in responses
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("uri")]
-        public string? Uri { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("sizeBytes")]
+        public string? SizeBytes { get; set; }
 
         /// <summary>
         /// Output only. The timestamp of when the `File` was created.<br/>
@@ -85,25 +104,6 @@ namespace Google.Gemini
         public string? MimeType { get; set; }
 
         /// <summary>
-        /// Optional. The human-readable display name for the `File`. The display name must be no more than 512 characters in length, including spaces. Example: "Welcome Image"
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("displayName")]
-        public string? DisplayName { get; set; }
-
-        /// <summary>
-        /// Source of the File.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("source")]
-        [global::System.Text.Json.Serialization.JsonConverter(typeof(global::Google.Gemini.JsonConverters.FileSourceJsonConverter))]
-        public global::Google.Gemini.FileSource? Source { get; set; }
-
-        /// <summary>
-        /// Metadata for a video `File`.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("videoMetadata")]
-        public global::Google.Gemini.VideoFileMetadata? VideoMetadata { get; set; }
-
-        /// <summary>
         /// Additional properties that are not explicitly defined in the schema
         /// </summary>
         [global::System.Text.Json.Serialization.JsonExtensionData]
@@ -112,38 +112,47 @@ namespace Google.Gemini
         /// <summary>
         /// Initializes a new instance of the <see cref="File" /> class.
         /// </summary>
-        /// <param name="sha256Hash">
-        /// Output only. SHA-256 hash of the uploaded bytes.<br/>
+        /// <param name="source">
+        /// Source of the File.
+        /// </param>
+        /// <param name="updateTime">
+        /// Output only. The timestamp of when the `File` was last updated.<br/>
         /// Included only in responses
         /// </param>
         /// <param name="state">
         /// Output only. Processing state of the File.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="downloadUri">
-        /// Output only. The download uri of the `File`.<br/>
-        /// Included only in responses
-        /// </param>
-        /// <param name="updateTime">
-        /// Output only. The timestamp of when the `File` was last updated.<br/>
-        /// Included only in responses
-        /// </param>
         /// <param name="error">
         /// The `Status` type defines a logical error model that is suitable for different programming environments, including REST APIs and RPC APIs. It is used by [gRPC](https://github.com/grpc). Each `Status` message contains three pieces of data: error code, error message, and error details. You can find out more about this error model and how to work with it in the [API Design Guide](https://cloud.google.com/apis/design/errors).
+        /// </param>
+        /// <param name="sha256Hash">
+        /// Output only. SHA-256 hash of the uploaded bytes.<br/>
+        /// Included only in responses
         /// </param>
         /// <param name="name">
         /// Immutable. Identifier. The `File` resource name. The ID (name excluding the "files/" prefix) can contain up to 40 characters that are lowercase alphanumeric or dashes (-). The ID cannot start or end with a dash. If the name is empty on create, a unique name will be generated. Example: `files/123-456`
         /// </param>
-        /// <param name="sizeBytes">
-        /// Output only. Size of the file in bytes.<br/>
+        /// <param name="videoMetadata">
+        /// Metadata for a video `File`.
+        /// </param>
+        /// <param name="displayName">
+        /// Optional. The human-readable display name for the `File`. The display name must be no more than 512 characters in length, including spaces. Example: "Welcome Image"
+        /// </param>
+        /// <param name="uri">
+        /// Output only. The uri of the `File`.<br/>
+        /// Included only in responses
+        /// </param>
+        /// <param name="downloadUri">
+        /// Output only. The download uri of the `File`.<br/>
         /// Included only in responses
         /// </param>
         /// <param name="expirationTime">
         /// Output only. The timestamp of when the `File` will be deleted. Only set if the `File` is scheduled to expire.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="uri">
-        /// Output only. The uri of the `File`.<br/>
+        /// <param name="sizeBytes">
+        /// Output only. Size of the file in bytes.<br/>
         /// Included only in responses
         /// </param>
         /// <param name="createTime">
@@ -154,48 +163,39 @@ namespace Google.Gemini
         /// Output only. MIME type of the file.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="displayName">
-        /// Optional. The human-readable display name for the `File`. The display name must be no more than 512 characters in length, including spaces. Example: "Welcome Image"
-        /// </param>
-        /// <param name="source">
-        /// Source of the File.
-        /// </param>
-        /// <param name="videoMetadata">
-        /// Metadata for a video `File`.
-        /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public File(
-            byte[]? sha256Hash,
-            global::Google.Gemini.FileState? state,
-            string? downloadUri,
-            string? updateTime,
-            global::Google.Gemini.Status? error,
-            string? name,
-            string? sizeBytes,
-            string? expirationTime,
-            string? uri,
-            string? createTime,
-            string? mimeType,
-            string? displayName,
             global::Google.Gemini.FileSource? source,
-            global::Google.Gemini.VideoFileMetadata? videoMetadata)
+            string? updateTime,
+            global::Google.Gemini.FileState? state,
+            global::Google.Gemini.Status? error,
+            byte[]? sha256Hash,
+            string? name,
+            global::Google.Gemini.VideoFileMetadata? videoMetadata,
+            string? displayName,
+            string? uri,
+            string? downloadUri,
+            string? expirationTime,
+            string? sizeBytes,
+            string? createTime,
+            string? mimeType)
         {
-            this.Sha256Hash = sha256Hash;
-            this.State = state;
-            this.DownloadUri = downloadUri;
+            this.Source = source;
             this.UpdateTime = updateTime;
+            this.State = state;
             this.Error = error;
+            this.Sha256Hash = sha256Hash;
             this.Name = name;
-            this.SizeBytes = sizeBytes;
-            this.ExpirationTime = expirationTime;
+            this.VideoMetadata = videoMetadata;
+            this.DisplayName = displayName;
             this.Uri = uri;
+            this.DownloadUri = downloadUri;
+            this.ExpirationTime = expirationTime;
+            this.SizeBytes = sizeBytes;
             this.CreateTime = createTime;
             this.MimeType = mimeType;
-            this.DisplayName = displayName;
-            this.Source = source;
-            this.VideoMetadata = videoMetadata;
         }
 
         /// <summary>
