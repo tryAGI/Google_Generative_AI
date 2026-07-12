d87967ee1466d62a081964b1356564c295fd7759
Konstantin S. <havendv@gmail.com>
2026-07-12 06:35:34 +0400

    feat: Updated OpenAPI spec (#612)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.EmbedContentBatch.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.EmbedContentBatch.g.cs
@@ -9,11 +9,11 @@ namespace Google.Gemini
     public sealed partial class EmbedContentBatch
     {
         /// <summary>
-        /// Output only. The time at which the batch was last updated.<br/>
+        /// Output only. Identifier. Resource name of the batch. Format: `batches/{batch_id}`.<br/>
         /// Included only in responses
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("updateTime")]
-        public string? UpdateTime { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("name")]
+        public string? Name { get; set; }
 
         /// <summary>
         /// Output only. The state of the batch.<br/>
@@ -24,10 +24,17 @@ namespace Google.Gemini
         public global::Google.Gemini.EmbedContentBatchState? State { get; set; }
 
         /// <summary>
-        /// Stats about the batch.
+        /// Required. The user-defined name of this batch.
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("batchStats")]
-        public global::Google.Gemini.EmbedContentBatchStats? BatchStats { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("displayName")]
+        public string? DisplayName { get; set; }
+
+        /// <summary>
+        /// Output only. The time at which the batch was last updated.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("updateTime")]
+        public string? UpdateTime { get; set; }
 
         /// <summary>
         /// Optional. The priority of the batch. Batches with a higher priority value will be processed before batches with a lower priority value. Negative values are allowed. Default is 0.
@@ -43,32 +50,6 @@ namespace Google.Gemini
         public string? EndTime { get; set; }
 
         /// <summary>
-        /// Required. The user-defined name of this batch.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("displayName")]
-        public string? DisplayName { get; set; }
-
-        /// <summary>
-        /// Output only. The time at which the batch was created.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("createTime")]
-        public string? CreateTime { get; set; }
-
-        /// <summary>
-        /// Required. The name of the `Model` to use for generating the completion. Format: `models/{model}`.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("model")]
-        public string? Model { get; set; }
-
-        /// <summary>
-        /// Output only. Identifier. Resource name of the batch. Format: `batches/{batch_id}`.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("name")]
-        public string? Name { get; set; }
-
-        /// <summary>
         /// Configures the input to the batch request.
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("inputConfig")]
@@ -81,6 +62,25 @@ namespace Google.Gemini
         public global::Google.Gemini.EmbedContentBatchOutput? Output { get; set; }
 
         /// <summary>
+        /// Stats about the batch.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("batchStats")]
+        public global::Google.Gemini.EmbedContentBatchStats? BatchStats { get; set; }
+
+        /// <summary>
+        /// Required. The name of the `Model` to use for generating the completion. Format: `models/{model}`.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("model")]
+        public string? Model { get; set; }
+
+        /// <summary>
+        /// Output only. The time at which the batch was created.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("createTime")]
+        public string? CreateTime { get; set; }
+
+        /// <summary>
         /// Additional properties that are not explicitly defined in the schema
         /// </summary>
         [global::System.Text.Json.Serialization.JsonExtensionData]
@@ -89,16 +89,20 @@ namespace Google.Gemini
         /// <summary>
         /// Initializes a new instance of the <see cref="EmbedContentBatch" /> class.
         /// </summary>
-        /// <param name="updateTime">
-        /// Output only. The time at which the batch was last updated.<br/>
+        /// <param name="name">
+        /// Output only. Identifier. Resource name of the batch. Format: `batches/{batch_id}`.<br/>
         /// Included only in responses
         /// </param>
         /// <param name="state">
         /// Output only. The state of the batch.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="batchStats">
-        /// Stats about the batch.
+        /// <param name="displayName">
+        /// Required. The user-defined name of this batch.
+        /// </param>
+        /// <param name="updateTime">
+        /// Output only. The time at which the batch was last updated.<br/>
+        /// Included only in responses
         /// </param>
         /// <param name="priority">
         /// Optional. The priority of the batch. Batches with a higher priority value will be processed before batches with a lower priority value. Negative values are allowed. Default is 0.
@@ -107,53 +111,49 @@ namespace Google.Gemini
         /// Output only. The time at which the batch processing completed.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="displayName">
-        /// Required. The user-defined name of this batch.
-        /// </param>
-        /// <param name="createTime">
-        /// Output only. The time at which the batch was created.<br/>
-        /// Included only in responses
-        /// </param>
-        /// <param name="model">
-        /// Required. The name of the `Model` to use for generating the completion. Format: `models/{model}`.
-        /// </param>
-        /// <param name="name">
-        /// Output only. Identifier. Resource name of the batch. Format: `batches/{batch_id}`.<br/>
-        /// Included only in responses
-        /// </param>
         /// <param name="inputConfig">
         /// Configures the input to the batch request.
         /// </param>
         /// <param name="output">
         /// The output of a batch request. This is returned in the `AsyncBatchEmbedContentResponse` or the `EmbedContentBatch.output` field.
         /// </param>
+        /// <param name="batchStats">
+        /// Stats about the batch.
+        /// </param>
+        /// <param name="model">
+        /// Required. The name of the `Model` to use for generating the completion. Format: `models/{model}`.
+        /// </param>
+        /// <param name="createTime">
+        /// Output only. The time at which the batch was created.<br/>
+        /// Included only in responses
+        /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public EmbedContentBatch(
-            string? updateTime,
+            string? name,
             global::Google.Gemini.EmbedContentBatchState? state,
-            global::Google.Gemini.EmbedContentBatchStats? batchStats,
+            string? displayName,
+            string? updateTime,
             string? priority,
             string? endTime,
-            string? displayName,
-            string? createTime,
-            string? model,
-            string? name,
             global::Google.Gemini.InputEmbedContentConfig? inputConfig,
-            global::Google.Gemini.EmbedContentBatchOutput? output)
+            global::Google.Gemini.EmbedContentBatchOutput? output,
+            global::Google.Gemini.EmbedContentBatchStats? batchStats,
+            string? model,
+            string? createTime)
         {
-            this.UpdateTime = updateTime;
+            this.Name = name;
             this.State = state;
-            this.BatchStats = batchStats;
+            this.DisplayName = displayName;
+            this.UpdateTime = updateTime;
             this.Priority = priority;
             this.EndTime = endTime;
-            this.DisplayName = displayName;
-            this.CreateTime = createTime;
-            this.Model = model;
-            this.Name = name;
             this.InputConfig = inputConfig;
             this.Output = output;
+            this.BatchStats = batchStats;
+            this.Model = model;
+            this.CreateTime = createTime;
         }
 
         /// <summary>
--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.GeneratedFile.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.GeneratedFile.g.cs
@@ -15,10 +15,10 @@ namespace Google.Gemini
         public global::Google.Gemini.Status? Error { get; set; }
 
         /// <summary>
-        /// Identifier. The name of the generated file. Example: `generatedFiles/abc-123`
+        /// MIME type of the generatedFile.
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("name")]
-        public string? Name { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("mimeType")]
+        public string? MimeType { get; set; }
 
         /// <summary>
         /// Output only. The state of the GeneratedFile.<br/>
@@ -29,10 +29,10 @@ namespace Google.Gemini
         public global::Google.Gemini.GeneratedFileState? State { get; set; }
 
         /// <summary>
-        /// MIME type of the generatedFile.
+        /// Identifier. The name of the generated file. Example: `generatedFiles/abc-123`
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("mimeType")]
-        public string? MimeType { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("name")]
+        public string? Name { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -46,29 +46,29 @@ namespace Google.Gemini
         /// <param name="error">
         /// The `Status` type defines a logical error model that is suitable for different programming environments, including REST APIs and RPC APIs. It is used by [gRPC](https://github.com/grpc). Each `Status` message contains three pieces of data: error code, error message, and error details. You can find out more about this error model and how to work with it in the [API Design Guide](https://cloud.google.com/apis/design/errors).
         /// </param>
-        /// <param name="name">
-        /// Identifier. The name of the generated file. Example: `generatedFiles/abc-123`
+        /// <param name="mimeType">
+        /// MIME type of the generatedFile.
         /// </param>
         /// <param name="state">
         /// Output only. The state of the GeneratedFile.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="mimeType">
-        /// MIME type of the generatedFile.
+        /// <param name="name">
+        /// Identifier. The name of the generated file. Example: `generatedFiles/abc-123`
         /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public GeneratedFile(
             global::Google.Gemini.Status? error,
-            string? name,
+            string? mimeType,
             global::Google.Gemini.GeneratedFileState? state,
-            string? mimeType)
+            string? name)
         {
             this.Error = error;
-            this.Name = name;
-            this.State = state;
             this.MimeType = mimeType;
+            this.State = state;
+            this.Name = name;
         }
 
         /// <summary>
