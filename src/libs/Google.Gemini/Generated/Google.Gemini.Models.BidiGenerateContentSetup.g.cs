4c7a97d5d73f0fd08eb8c566a2ea2a271599c7a0
Konstantin S. <havendv@gmail.com>
2026-07-27 01:58:04 +0400

    feat: Updated OpenAPI spec (#729)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.BidiGenerateContentSetup.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.BidiGenerateContentSetup.g.cs
@@ -9,6 +9,12 @@ namespace Google.Gemini
     public sealed partial class BidiGenerateContentSetup
     {
         /// <summary>
+        /// The audio transcription configuration.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("inputAudioTranscription")]
+        public global::Google.Gemini.AudioTranscriptionConfig? InputAudioTranscription { get; set; }
+
+        /// <summary>
         /// Optional. A list of `Tools` the model may use to generate the next response. A `Tool` is a piece of code that enables the system to interact with external systems to perform an action, or set of actions, outside of knowledge and scope of the model.
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("tools")]
@@ -33,10 +39,10 @@ namespace Google.Gemini
         public string? Model { get; set; }
 
         /// <summary>
-        /// History configuration. This message is included in the session configuration as `BidiGenerateContentSetup.history_config`. Configures the exchange of history messages.
+        /// Session resumption configuration. This message is included in the session configuration as `BidiGenerateContentSetup.session_resumption`. If configured, the server will send `SessionResumptionUpdate` messages.
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("historyConfig")]
-        public global::Google.Gemini.HistoryConfig? HistoryConfig { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("sessionResumption")]
+        public global::Google.Gemini.SessionResumptionConfig? SessionResumption { get; set; }
 
         /// <summary>
         /// The audio transcription configuration.
@@ -45,28 +51,22 @@ namespace Google.Gemini
         public global::Google.Gemini.AudioTranscriptionConfig? OutputAudioTranscription { get; set; }
 
         /// <summary>
-        /// Configuration options for model generation and outputs. Not all parameters are configurable for every model.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("generationConfig")]
-        public global::Google.Gemini.GenerationConfig? GenerationConfig { get; set; }
-
-        /// <summary>
-        /// Session resumption configuration. This message is included in the session configuration as `BidiGenerateContentSetup.session_resumption`. If configured, the server will send `SessionResumptionUpdate` messages.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("sessionResumption")]
-        public global::Google.Gemini.SessionResumptionConfig? SessionResumption { get; set; }
-
-        /// <summary>
         /// The base structured datatype containing multi-part content of a message. A `Content` includes a `role` field designating the producer of the `Content` and a `parts` field containing multi-part data that contains the content of the message turn.
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("systemInstruction")]
         public global::Google.Gemini.Content? SystemInstruction { get; set; }
 
         /// <summary>
-        /// The audio transcription configuration.
+        /// History configuration. This message is included in the session configuration as `BidiGenerateContentSetup.history_config`. Configures the exchange of history messages.
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("inputAudioTranscription")]
-        public global::Google.Gemini.AudioTranscriptionConfig? InputAudioTranscription { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("historyConfig")]
+        public global::Google.Gemini.HistoryConfig? HistoryConfig { get; set; }
+
+        /// <summary>
+        /// Configuration options for model generation and outputs. Not all parameters are configurable for every model.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("generationConfig")]
+        public global::Google.Gemini.GenerationConfig? GenerationConfig { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -77,6 +77,9 @@ namespace Google.Gemini
         /// <summary>
         /// Initializes a new instance of the <see cref="BidiGenerateContentSetup" /> class.
         /// </summary>
+        /// <param name="inputAudioTranscription">
+        /// The audio transcription configuration.
+        /// </param>
         /// <param name="tools">
         /// Optional. A list of `Tools` the model may use to generate the next response. A `Tool` is a piece of code that enables the system to interact with external systems to perform an action, or set of actions, outside of knowledge and scope of the model.
         /// </param>
@@ -89,49 +92,46 @@ namespace Google.Gemini
         /// <param name="model">
         /// Required. The model's resource name. This serves as an ID for the Model to use. Format: `models/{model}`
         /// </param>
-        /// <param name="historyConfig">
-        /// History configuration. This message is included in the session configuration as `BidiGenerateContentSetup.history_config`. Configures the exchange of history messages.
+        /// <param name="sessionResumption">
+        /// Session resumption configuration. This message is included in the session configuration as `BidiGenerateContentSetup.session_resumption`. If configured, the server will send `SessionResumptionUpdate` messages.
         /// </param>
         /// <param name="outputAudioTranscription">
         /// The audio transcription configuration.
         /// </param>
-        /// <param name="generationConfig">
-        /// Configuration options for model generation and outputs. Not all parameters are configurable for every model.
-        /// </param>
-        /// <param name="sessionResumption">
-        /// Session resumption configuration. This message is included in the session configuration as `BidiGenerateContentSetup.session_resumption`. If configured, the server will send `SessionResumptionUpdate` messages.
-        /// </param>
         /// <param name="systemInstruction">
         /// The base structured datatype containing multi-part content of a message. A `Content` includes a `role` field designating the producer of the `Content` and a `parts` field containing multi-part data that contains the content of the message turn.
         /// </param>
-        /// <param name="inputAudioTranscription">
-        /// The audio transcription configuration.
+        /// <param name="historyConfig">
+        /// History configuration. This message is included in the session configuration as `BidiGenerateContentSetup.history_config`. Configures the exchange of history messages.
+        /// </param>
+        /// <param name="generationConfig">
+        /// Configuration options for model generation and outputs. Not all parameters are configurable for every model.
         /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public BidiGenerateContentSetup(
+            global::Google.Gemini.AudioTranscriptionConfig? inputAudioTranscription,
             global::System.Collections.Generic.IList<global::Google.Gemini.Tool>? tools,
             global::Google.Gemini.RealtimeInputConfig? realtimeInputConfig,
             global::Google.Gemini.ContextWindowCompressionConfig? contextWindowCompression,
             string? model,
-            global::Google.Gemini.HistoryConfig? historyConfig,
-            global::Google.Gemini.AudioTranscriptionConfig? outputAudioTranscription,
-            global::Google.Gemini.GenerationConfig? generationConfig,
             global::Google.Gemini.SessionResumptionConfig? sessionResumption,
+            global::Google.Gemini.AudioTranscriptionConfig? outputAudioTranscription,
             global::Google.Gemini.Content? systemInstruction,
-            global::Google.Gemini.AudioTranscriptionConfig? inputAudioTranscription)
+            global::Google.Gemini.HistoryConfig? historyConfig,
+            global::Google.Gemini.GenerationConfig? generationConfig)
         {
+            this.InputAudioTranscription = inputAudioTranscription;
             this.Tools = tools;
             this.RealtimeInputConfig = realtimeInputConfig;
             this.ContextWindowCompression = contextWindowCompression;
             this.Model = model;
-            this.HistoryConfig = historyConfig;
-            this.OutputAudioTranscription = outputAudioTranscription;
-            this.GenerationConfig = generationConfig;
             this.SessionResumption = sessionResumption;
+            this.OutputAudioTranscription = outputAudioTranscription;
             this.SystemInstruction = systemInstruction;
-            this.InputAudioTranscription = inputAudioTranscription;
+            this.HistoryConfig = historyConfig;
+            this.GenerationConfig = generationConfig;
         }
 
         /// <summary>
