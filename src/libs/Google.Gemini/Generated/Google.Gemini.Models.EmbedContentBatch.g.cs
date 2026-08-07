274c042ee79fe4acf0b8d4b1c7fbcf6924f8201e
Konstantin S. <havendv@gmail.com>
2026-08-08 01:37:11 +0400

    feat: Updated OpenAPI spec (#820)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.EmbedContentBatch.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.EmbedContentBatch.g.cs
@@ -9,17 +9,30 @@ namespace Google.Gemini
     public sealed partial class EmbedContentBatch
     {
         /// <summary>
-        /// Configures the input to the batch request.
+        /// Required. The user-defined name of this batch.
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("inputConfig")]
-        public global::Google.Gemini.InputEmbedContentConfig? InputConfig { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("displayName")]
+        public string? DisplayName { get; set; }
 
         /// <summary>
-        /// Output only. The time at which the batch was last updated.<br/>
+        /// Output only. Identifier. Resource name of the batch. Format: `batches/{batch_id}`.<br/>
         /// Included only in responses
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("updateTime")]
-        public string? UpdateTime { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// The output of a batch request. This is returned in the `AsyncBatchEmbedContentResponse` or the `EmbedContentBatch.output` field.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("output")]
+        public global::Google.Gemini.EmbedContentBatchOutput? Output { get; set; }
+
+        /// <summary>
+        /// Output only. The time at which the batch was created.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("createTime")]
+        public string? CreateTime { get; set; }
 
         /// <summary>
         /// Output only. The state of the batch.<br/>
@@ -37,18 +50,6 @@ namespace Google.Gemini
         public string? EndTime { get; set; }
 
         /// <summary>
-        /// Optional. The priority of the batch. Batches with a higher priority value will be processed before batches with a lower priority value. Negative values are allowed. Default is 0.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("priority")]
-        public string? Priority { get; set; }
-
-        /// <summary>
-        /// The output of a batch request. This is returned in the `AsyncBatchEmbedContentResponse` or the `EmbedContentBatch.output` field.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("output")]
-        public global::Google.Gemini.EmbedContentBatchOutput? Output { get; set; }
-
-        /// <summary>
         /// Stats about the batch.
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("batchStats")]
@@ -61,24 +62,23 @@ namespace Google.Gemini
         public string? Model { get; set; }
 
         /// <summary>
-        /// Output only. The time at which the batch was created.<br/>
+        /// Output only. The time at which the batch was last updated.<br/>
         /// Included only in responses
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("createTime")]
-        public string? CreateTime { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("updateTime")]
+        public string? UpdateTime { get; set; }
 
         /// <summary>
-        /// Required. The user-defined name of this batch.
+        /// Configures the input to the batch request.
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("displayName")]
-        public string? DisplayName { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("inputConfig")]
+        public global::Google.Gemini.InputEmbedContentConfig? InputConfig { get; set; }
 
         /// <summary>
-        /// Output only. Identifier. Resource name of the batch. Format: `batches/{batch_id}`.<br/>
-        /// Included only in responses
+        /// Optional. The priority of the batch. Batches with a higher priority value will be processed before batches with a lower priority value. Negative values are allowed. Default is 0.
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("name")]
-        public string? Name { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("priority")]
+        public string? Priority { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -89,11 +89,18 @@ namespace Google.Gemini
         /// <summary>
         /// Initializes a new instance of the <see cref="EmbedContentBatch" /> class.
         /// </summary>
-        /// <param name="inputConfig">
-        /// Configures the input to the batch request.
+        /// <param name="displayName">
+        /// Required. The user-defined name of this batch.
         /// </param>
-        /// <param name="updateTime">
-        /// Output only. The time at which the batch was last updated.<br/>
+        /// <param name="name">
+        /// Output only. Identifier. Resource name of the batch. Format: `batches/{batch_id}`.<br/>
+        /// Included only in responses
+        /// </param>
+        /// <param name="output">
+        /// The output of a batch request. This is returned in the `AsyncBatchEmbedContentResponse` or the `EmbedContentBatch.output` field.
+        /// </param>
+        /// <param name="createTime">
+        /// Output only. The time at which the batch was created.<br/>
         /// Included only in responses
         /// </param>
         /// <param name="state">
@@ -104,56 +111,49 @@ namespace Google.Gemini
         /// Output only. The time at which the batch processing completed.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="priority">
-        /// Optional. The priority of the batch. Batches with a higher priority value will be processed before batches with a lower priority value. Negative values are allowed. Default is 0.
-        /// </param>
-        /// <param name="output">
-        /// The output of a batch request. This is returned in the `AsyncBatchEmbedContentResponse` or the `EmbedContentBatch.output` field.
-        /// </param>
         /// <param name="batchStats">
         /// Stats about the batch.
         /// </param>
         /// <param name="model">
         /// Required. The name of the `Model` to use for generating the completion. Format: `models/{model}`.
         /// </param>
-        /// <param name="createTime">
-        /// Output only. The time at which the batch was created.<br/>
+        /// <param name="updateTime">
+        /// Output only. The time at which the batch was last updated.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="displayName">
-        /// Required. The user-defined name of this batch.
+        /// <param name="inputConfig">
+        /// Configures the input to the batch request.
         /// </param>
-        /// <param name="name">
-        /// Output only. Identifier. Resource name of the batch. Format: `batches/{batch_id}`.<br/>
-        /// Included only in responses
+        /// <param name="priority">
+        /// Optional. The priority of the batch. Batches with a higher priority value will be processed before batches with a lower priority value. Negative values are allowed. Default is 0.
         /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public EmbedContentBatch(
-            global::Google.Gemini.InputEmbedContentConfig? inputConfig,
-            string? updateTime,
+            string? displayName,
+            string? name,
+            global::Google.Gemini.EmbedContentBatchOutput? output,
+            string? createTime,
             global::Google.Gemini.EmbedContentBatchState? state,
             string? endTime,
-            string? priority,
-            global::Google.Gemini.EmbedContentBatchOutput? output,
             global::Google.Gemini.EmbedContentBatchStats? batchStats,
             string? model,
-            string? createTime,
-            string? displayName,
-            string? name)
+            string? updateTime,
+            global::Google.Gemini.InputEmbedContentConfig? inputConfig,
+            string? priority)
         {
-            this.InputConfig = inputConfig;
-            this.UpdateTime = updateTime;
-            this.State = state;
-            this.EndTime = endTime;
-            this.Priority = priority;
-            this.Output = output;
-            this.BatchStats = batchStats;
-            this.Model = model;
-            this.CreateTime = createTime;
             this.DisplayName = displayName;
             this.Name = name;
+            this.Output = output;
+            this.CreateTime = createTime;
+            this.State = state;
+            this.EndTime = endTime;
+            this.BatchStats = batchStats;
+            this.Model = model;
+            this.UpdateTime = updateTime;
+            this.InputConfig = inputConfig;
+            this.Priority = priority;
         }
 
         /// <summary>
--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.Environment.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.Environment.g.cs
@@ -9,25 +9,12 @@ namespace Google.Gemini
     public sealed partial class Environment
     {
         /// <summary>
-        /// Output only. The number of files in the environment, output only.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("fileCount")]
-        public string? FileCount { get; set; }
-
-        /// <summary>
         /// Output only. The status of the environment container.<br/>
         /// Included only in responses
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("status")]
         [global::System.Text.Json.Serialization.JsonConverter(typeof(global::Google.Gemini.JsonConverters.EnvironmentStatusJsonConverter))]
         public global::Google.Gemini.EnvironmentStatus? Status { get; set; }
-
-        /// <summary>
-        /// Sources to be mounted into the environment.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("sources")]
-        public global::System.Collections.Generic.IList<global::Google.Gemini.Source>? Sources { get; set; }
 
         /// <summary>
         /// Output only. The total size of the environment files in bytes, output only.<br/>
@@ -37,18 +24,30 @@ namespace Google.Gemini
         public string? SizeBytes { get; set; }
 
         /// <summary>
+        /// Sources to be mounted into the environment.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("sources")]
+        public global::System.Collections.Generic.IList<global::Google.Gemini.Source>? Sources { get; set; }
+
+        /// <summary>
+        /// Output only. The time at which the environment was last updated in ISO 8601 format (YYYY-MM-DDThh:mm:ssZ).<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("updated")]
+        public string? Updated { get; set; }
+
+        /// <summary>
+        /// Network egress configuration for the environment.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("networkAllowlist")]
+        public global::Google.Gemini.EnvironmentNetworkEgressAllowlist? NetworkAllowlist { get; set; }
+
+        /// <summary>
         /// Output only. The time at which the environment was last accessed in ISO 8601 format (YYYY-MM-DDThh:mm:ssZ).<br/>
         /// Included only in responses
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("lastAccessed")]
         public string? LastAccessed { get; set; }
-
-        /// <summary>
-        /// Output only. The time at which the environment was created in ISO 8601 format (YYYY-MM-DDThh:mm:ssZ).<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("created")]
-        public string? Created { get; set; }
 
         /// <summary>
         /// Network egress mode.
@@ -65,17 +64,18 @@ namespace Google.Gemini
         public string? Id { get; set; }
 
         /// <summary>
-        /// Output only. The time at which the environment was last updated in ISO 8601 format (YYYY-MM-DDThh:mm:ssZ).<br/>
+        /// Output only. The time at which the environment was created in ISO 8601 format (YYYY-MM-DDThh:mm:ssZ).<br/>
         /// Included only in responses
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("updated")]
-        public string? Updated { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("created")]
+        public string? Created { get; set; }
 
         /// <summary>
-        /// Network egress configuration for the environment.
+        /// Output only. The number of files in the environment, output only.<br/>
+        /// Included only in responses
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("networkAllowlist")]
-        public global::Google.Gemini.EnvironmentNetworkEgressAllowlist? NetworkAllowlist { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("fileCount")]
+        public string? FileCount { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -86,27 +86,26 @@ namespace Google.Gemini
         /// <summary>
         /// Initializes a new instance of the <see cref="Environment" /> class.
         /// </summary>
-        /// <param name="fileCount">
-        /// Output only. The number of files in the environment, output only.<br/>
-        /// Included only in responses
-        /// </param>
         /// <param name="status">
         /// Output only. The status of the environment container.<br/>
         /// Included only in responses
-        /// </param>
-        /// <param name="sources">
-        /// Sources to be mounted into the environment.
         /// </param>
         /// <param name="sizeBytes">
         /// Output only. The total size of the environment files in bytes, output only.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="lastAccessed">
-        /// Output only. The time at which the environment was last accessed in ISO 8601 format (YYYY-MM-DDThh:mm:ssZ).<br/>
+        /// <param name="sources">
+        /// Sources to be mounted into the environment.
+        /// </param>
+        /// <param name="updated">
+        /// Output only. The time at which the environment was last updated in ISO 8601 format (YYYY-MM-DDThh:mm:ssZ).<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="created">
-        /// Output only. The time at which the environment was created in ISO 8601 format (YYYY-MM-DDThh:mm:ssZ).<br/>
+        /// <param name="networkAllowlist">
+        /// Network egress configuration for the environment.
+        /// </param>
+        /// <param name="lastAccessed">
+        /// Output only. The time at which the environment was last accessed in ISO 8601 format (YYYY-MM-DDThh:mm:ssZ).<br/>
         /// Included only in responses
         /// </param>
         /// <param name="networkMode">
@@ -116,38 +115,39 @@ namespace Google.Gemini
         /// Required. Output only. The ID of the environment.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="updated">
-        /// Output only. The time at which the environment was last updated in ISO 8601 format (YYYY-MM-DDThh:mm:ssZ).<br/>
+        /// <param name="created">
+        /// Output only. The time at which the environment was created in ISO 8601 format (YYYY-MM-DDThh:mm:ssZ).<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="networkAllowlist">
-        /// Network egress configuration for the environment.
+        /// <param name="fileCount">
+        /// Output only. The number of files in the environment, output only.<br/>
+        /// Included only in responses
         /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public Environment(
-            string? fileCount,
             global::Google.Gemini.EnvironmentStatus? status,
-            global::System.Collections.Generic.IList<global::Google.Gemini.Source>? sources,
             string? sizeBytes,
+            global::System.Collections.Generic.IList<global::Google.Gemini.Source>? sources,
+            string? updated,
+            global::Google.Gemini.EnvironmentNetworkEgressAllowlist? networkAllowlist,
             string? lastAccessed,
-            string? created,
             global::Google.Gemini.EnvironmentNetworkMode? networkMode,
             string? id,
-            string? updated,
-            global::Google.Gemini.EnvironmentNetworkEgressAllowlist? networkAllowlist)
+            string? created,
+            string? fileCount)
         {
-            this.FileCount = fileCount;
             this.Status = status;
-            this.Sources = sources;
             this.SizeBytes = sizeBytes;
-            this.LastAccessed = lastAccessed;
-            this.Created = created;
-            this.NetworkMode = networkMode;
-            this.Id = id;
+            this.Sources = sources;
             this.Updated = updated;
             this.NetworkAllowlist = networkAllowlist;
+            this.LastAccessed = lastAccessed;
+            this.NetworkMode = networkMode;
+            this.Id = id;
+            this.Created = created;
+            this.FileCount = fileCount;
         }
 
         /// <summary>
