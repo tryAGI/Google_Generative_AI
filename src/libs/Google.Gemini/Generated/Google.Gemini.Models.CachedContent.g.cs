63662a2f0acc3f38f520ea4c5ba37f766ba983ae
Konstantin S. <havendv@gmail.com>
2026-08-19 13:32:14 +0400

    feat: Updated OpenAPI spec (#912)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.CachedContent.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.CachedContent.g.cs
@@ -15,23 +15,11 @@ namespace Google.Gemini
         public string? ExpireTime { get; set; }
 
         /// <summary>
-        /// Optional. Immutable. The user-generated meaningful display name of the cached content. Maximum 128 Unicode characters.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("displayName")]
-        public string? DisplayName { get; set; }
-
-        /// <summary>
-        /// Output only. Creation time of the cache entry.<br/>
+        /// Output only. When the cache entry was last updated in UTC time.<br/>
         /// Included only in responses
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("createTime")]
-        public string? CreateTime { get; set; }
-
-        /// <summary>
-        /// Metadata on the usage of the cached content.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("usageMetadata")]
-        public global::Google.Gemini.CachedContentUsageMetadata? UsageMetadata { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("updateTime")]
+        public string? UpdateTime { get; set; }
 
         /// <summary>
         /// Optional. Input only. Immutable. A list of `Tools` the model may use to generate the next response
@@ -47,11 +35,23 @@ namespace Google.Gemini
         public string? Name { get; set; }
 
         /// <summary>
-        /// Output only. When the cache entry was last updated in UTC time.<br/>
+        /// Input only. New TTL for this resource, input only.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("ttl")]
+        public string? Ttl { get; set; }
+
+        /// <summary>
+        /// Output only. Creation time of the cache entry.<br/>
         /// Included only in responses
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("updateTime")]
-        public string? UpdateTime { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("createTime")]
+        public string? CreateTime { get; set; }
+
+        /// <summary>
+        /// The base structured datatype containing multi-part content of a message. A `Content` includes a `role` field designating the producer of the `Content` and a `parts` field containing multi-part data that contains the content of the message turn.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("systemInstruction")]
+        public global::Google.Gemini.Content? SystemInstruction { get; set; }
 
         /// <summary>
         /// Required. Immutable. The name of the `Model` to use for cached content Format: `models/{model}`
@@ -60,28 +60,28 @@ namespace Google.Gemini
         public string? Model { get; set; }
 
         /// <summary>
+        /// Metadata on the usage of the cached content.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("usageMetadata")]
+        public global::Google.Gemini.CachedContentUsageMetadata? UsageMetadata { get; set; }
+
+        /// <summary>
         /// The Tool configuration containing parameters for specifying `Tool` use in the request.
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("toolConfig")]
         public global::Google.Gemini.ToolConfig? ToolConfig { get; set; }
 
         /// <summary>
+        /// Optional. Immutable. The user-generated meaningful display name of the cached content. Maximum 128 Unicode characters.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("displayName")]
+        public string? DisplayName { get; set; }
+
+        /// <summary>
         /// Optional. Input only. Immutable. The content to cache.
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("contents")]
         public global::System.Collections.Generic.IList<global::Google.Gemini.Content>? Contents { get; set; }
-
-        /// <summary>
-        /// Input only. New TTL for this resource, input only.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("ttl")]
-        public string? Ttl { get; set; }
-
-        /// <summary>
-        /// The base structured datatype containing multi-part content of a message. A `Content` includes a `role` field designating the producer of the `Content` and a `parts` field containing multi-part data that contains the content of the message turn.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("systemInstruction")]
-        public global::Google.Gemini.Content? SystemInstruction { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -95,15 +95,9 @@ namespace Google.Gemini
         /// <param name="expireTime">
         /// Timestamp in UTC of when this resource is considered expired. This is *always* provided on output, regardless of what was sent on input.
         /// </param>
-        /// <param name="displayName">
-        /// Optional. Immutable. The user-generated meaningful display name of the cached content. Maximum 128 Unicode characters.
-        /// </param>
-        /// <param name="createTime">
-        /// Output only. Creation time of the cache entry.<br/>
+        /// <param name="updateTime">
+        /// Output only. When the cache entry was last updated in UTC time.<br/>
         /// Included only in responses
-        /// </param>
-        /// <param name="usageMetadata">
-        /// Metadata on the usage of the cached content.
         /// </param>
         /// <param name="tools">
         /// Optional. Input only. Immutable. A list of `Tools` the model may use to generate the next response
@@ -112,54 +106,60 @@ namespace Google.Gemini
         /// Output only. Identifier. The resource name referring to the cached content. Format: `cachedContents/{id}`<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="updateTime">
-        /// Output only. When the cache entry was last updated in UTC time.<br/>
+        /// <param name="ttl">
+        /// Input only. New TTL for this resource, input only.
+        /// </param>
+        /// <param name="createTime">
+        /// Output only. Creation time of the cache entry.<br/>
         /// Included only in responses
+        /// </param>
+        /// <param name="systemInstruction">
+        /// The base structured datatype containing multi-part content of a message. A `Content` includes a `role` field designating the producer of the `Content` and a `parts` field containing multi-part data that contains the content of the message turn.
         /// </param>
         /// <param name="model">
         /// Required. Immutable. The name of the `Model` to use for cached content Format: `models/{model}`
         /// </param>
+        /// <param name="usageMetadata">
+        /// Metadata on the usage of the cached content.
+        /// </param>
         /// <param name="toolConfig">
         /// The Tool configuration containing parameters for specifying `Tool` use in the request.
         /// </param>
+        /// <param name="displayName">
+        /// Optional. Immutable. The user-generated meaningful display name of the cached content. Maximum 128 Unicode characters.
+        /// </param>
         /// <param name="contents">
         /// Optional. Input only. Immutable. The content to cache.
-        /// </param>
-        /// <param name="ttl">
-        /// Input only. New TTL for this resource, input only.
-        /// </param>
-        /// <param name="systemInstruction">
-        /// The base structured datatype containing multi-part content of a message. A `Content` includes a `role` field designating the producer of the `Content` and a `parts` field containing multi-part data that contains the content of the message turn.
         /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public CachedContent(
             string? expireTime,
-            string? displayName,
-            string? createTime,
-            global::Google.Gemini.CachedContentUsageMetadata? usageMetadata,
+            string? updateTime,
             global::System.Collections.Generic.IList<global::Google.Gemini.Tool>? tools,
             string? name,
-            string? updateTime,
-            string? model,
-            global::Google.Gemini.ToolConfig? toolConfig,
-            global::System.Collections.Generic.IList<global::Google.Gemini.Content>? contents,
             string? ttl,
-            global::Google.Gemini.Content? systemInstruction)
+            string? createTime,
+            global::Google.Gemini.Content? systemInstruction,
+            string? model,
+            global::Google.Gemini.CachedContentUsageMetadata? usageMetadata,
+            global::Google.Gemini.ToolConfig? toolConfig,
+            string? displayName,
+            global::System.Collections.Generic.IList<global::Google.Gemini.Content>? contents)
         {
             this.ExpireTime = expireTime;
-            this.DisplayName = displayName;
-            this.CreateTime = createTime;
-            this.UsageMetadata = usageMetadata;
+            this.UpdateTime = updateTime;
             this.Tools = tools;
             this.Name = name;
-            this.UpdateTime = updateTime;
-            this.Model = model;
-            this.ToolConfig = toolConfig;
-            this.Contents = contents;
             this.Ttl = ttl;
+            this.CreateTime = createTime;
             this.SystemInstruction = systemInstruction;
+            this.Model = model;
+            this.UsageMetadata = usageMetadata;
+            this.ToolConfig = toolConfig;
+            this.DisplayName = displayName;
+            this.Contents = contents;
         }
 
         /// <summary>
--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.Candidate.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.Candidate.g.cs
@@ -9,6 +9,13 @@ namespace Google.Gemini
     public sealed partial class Candidate
     {
         /// <summary>
+        /// Output only. Index of the candidate in the list of response candidates.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("index")]
+        public int? Index { get; set; }
+
+        /// <summary>
         /// A collection of source attributions for a piece of content.
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("citationMetadata")]
@@ -21,17 +28,16 @@ namespace Google.Gemini
         public global::Google.Gemini.GroundingMetadata? GroundingMetadata { get; set; }
 
         /// <summary>
-        /// Optional. Output only. Details the reason why the model stopped generating tokens. This is populated only when `finish_reason` is set.<br/>
-        /// Included only in responses
+        /// The base structured datatype containing multi-part content of a message. A `Content` includes a `role` field designating the producer of the `Content` and a `parts` field containing multi-part data that contains the content of the message turn.
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("finishMessage")]
-        public string? FinishMessage { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("content")]
+        public global::Google.Gemini.Content? Content { get; set; }
 
         /// <summary>
-        /// Logprobs Result
+        /// List of ratings for the safety of a response candidate. There is at most one rating per category.
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("logprobsResult")]
-        public global::Google.Gemini.LogprobsResult? LogprobsResult { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("safetyRatings")]
+        public global::System.Collections.Generic.IList<global::Google.Gemini.SafetyRating>? SafetyRatings { get; set; }
 
         /// <summary>
         /// Output only. Token count for this candidate.<br/>
@@ -41,6 +47,12 @@ namespace Google.Gemini
         public int? TokenCount { get; set; }
 
         /// <summary>
+        /// Logprobs Result
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("logprobsResult")]
+        public global::Google.Gemini.LogprobsResult? LogprobsResult { get; set; }
+
+        /// <summary>
         /// Output only. Attribution information for sources that contributed to a grounded answer. This field is populated for `GenerateAnswer` calls.<br/>
         /// Included only in responses
         /// </summary>
@@ -48,16 +60,24 @@ namespace Google.Gemini
         public global::System.Collections.Generic.IList<global::Google.Gemini.GroundingAttribution>? GroundingAttributions { get; set; }
 
         /// <summary>
+        /// Optional. Output only. Details the reason why the model stopped generating tokens. This is populated only when `finish_reason` is set.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("finishMessage")]
+        public string? FinishMessage { get; set; }
+
+        /// <summary>
+        /// Output only. Average log probability score of the candidate.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("avgLogprobs")]
+        public double? AvgLogprobs { get; set; }
+
+        /// <summary>
         /// Metadata related to url context retrieval tool.
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("urlContextMetadata")]
         public global::Google.Gemini.UrlContextMetadata? UrlContextMetadata { get; set; }
-
-        /// <summary>
-        /// The base structured datatype containing multi-part content of a message. A `Content` includes a `role` field designating the producer of the `Content` and a `parts` field containing multi-part data that contains the content of the message turn.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("content")]
-        public global::Google.Gemini.Content? Content { get; set; }
 
         /// <summary>
         /// Optional. Output only. The reason why the model stopped generating tokens. If empty, the model has not stopped generating tokens.<br/>
@@ -68,26 +88,6 @@ namespace Google.Gemini
         public global::Google.Gemini.CandidateFinishReason? FinishReason { get; set; }
 
         /// <summary>
-        /// List of ratings for the safety of a response candidate. There is at most one rating per category.
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("safetyRatings")]
-        public global::System.Collections.Generic.IList<global::Google.Gemini.SafetyRating>? SafetyRatings { get; set; }
-
-        /// <summary>
-        /// Output only. Average log probability score of the candidate.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("avgLogprobs")]
-        public double? AvgLogprobs { get; set; }
-
-        /// <summary>
-        /// Output only. Index of the candidate in the list of response candidates.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("index")]
-        public int? Index { get; set; }
-
-        /// <summary>
         /// Additional properties that are not explicitly defined in the schema
         /// </summary>
         [global::System.Text.Json.Serialization.JsonExtensionData]
@@ -96,77 +96,77 @@ namespace Google.Gemini
         /// <summary>
         /// Initializes a new instance of the <see cref="Candidate" /> class.
         /// </summary>
+        /// <param name="index">
+        /// Output only. Index of the candidate in the list of response candidates.<br/>
+        /// Included only in responses
+        /// </param>
         /// <param name="citationMetadata">
         /// A collection of source attributions for a piece of content.
         /// </param>
         /// <param name="groundingMetadata">
         /// Metadata returned to client when grounding is enabled.
         /// </param>
-        /// <param name="finishMessage">
-        /// Optional. Output only. Details the reason why the model stopped generating tokens. This is populated only when `finish_reason` is set.<br/>
-        /// Included only in responses
+        /// <param name="content">
+        /// The base structured datatype containing multi-part content of a message. A `Content` includes a `role` field designating the producer of the `Content` and a `parts` field containing multi-part data that contains the content of the message turn.
         /// </param>
-        /// <param name="logprobsResult">
-        /// Logprobs Result
+        /// <param name="safetyRatings">
+        /// List of ratings for the safety of a response candidate. There is at most one rating per category.
         /// </param>
         /// <param name="tokenCount">
         /// Output only. Token count for this candidate.<br/>
         /// Included only in responses
         /// </param>
+        /// <param name="logprobsResult">
+        /// Logprobs Result
+        /// </param>
         /// <param name="groundingAttributions">
         /// Output only. Attribution information for sources that contributed to a grounded answer. This field is populated for `GenerateAnswer` calls.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="urlContextMetadata">
-        /// Metadata related to url context retrieval tool.
-        /// </param>
-        /// <param name="content">
-        /// The base structured datatype containing multi-part content of a message. A `Content` includes a `role` field designating the producer of the `Content` and a `parts` field containing multi-part data that contains the content of the message turn.
-        /// </param>
-        /// <param name="finishReason">
-        /// Optional. Output only. The reason why the model stopped generating tokens. If empty, the model has not stopped generating tokens.<br/>
+        /// <param name="finishMessage">
+        /// Optional. Output only. Details the reason why the model stopped generating tokens. This is populated only when `finish_reason` is set.<br/>
         /// Included only in responses
-        /// </param>
-        /// <param name="safetyRatings">
-        /// List of ratings for the safety of a response candidate. There is at most one rating per category.
         /// </param>
         /// <param name="avgLogprobs">
         /// Output only. Average log probability score of the candidate.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="index">
-        /// Output only. Index of the candidate in the list of response candidates.<br/>
+        /// <param name="urlContextMetadata">
+        /// Metadata related to url context retrieval tool.
+        /// </param>
+        /// <param name="finishReason">
+        /// Optional. Output only. The reason why the model stopped generating tokens. If empty, the model has not stopped generating tokens.<br/>
         /// Included only in responses
         /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public Candidate(
+            int? index,
             global::Google.Gemini.CitationMetadata? citationMetadata,
             global::Google.Gemini.GroundingMetadata? groundingMetadata,
-            string? finishMessage,
-            global::Google.Gemini.LogprobsResult? logprobsResult,
-            int? tokenCount,
-            global::System.Collections.Generic.IList<global::Google.Gemini.GroundingAttribution>? groundingAttributions,
-            global::Google.Gemini.UrlContextMetadata? urlContextMetadata,
             global::Google.Gemini.Content? content,
-            global::Google.Gemini.CandidateFinishReason? finishReason,
             global::System.Collections.Generic.IList<global::Google.Gemini.SafetyRating>? safetyRatings,
+            int? tokenCount,
+            global::Google.Gemini.LogprobsResult? logprobsResult,
+            global::System.Collections.Generic.IList<global::Google.Gemini.GroundingAttribution>? groundingAttributions,
+            string? finishMessage,
             double? avgLogprobs,
-            int? index)
+            global::Google.Gemini.UrlContextMetadata? urlContextMetadata,
+            global::Google.Gemini.CandidateFinishReason? finishReason)
         {
+            this.Index = index;
             this.CitationMetadata = citationMetadata;
             this.GroundingMetadata = groundingMetadata;
-            this.FinishMessage = finishMessage;
-            this.LogprobsResult = logprobsResult;
-            this.TokenCount = tokenCount;
-            this.GroundingAttributions = groundingAttributions;
-            this.UrlContextMetadata = urlContextMetadata;
             this.Content = content;
-            this.FinishReason = finishReason;
             this.SafetyRatings = safetyRatings;
+            this.TokenCount = tokenCount;
+            this.LogprobsResult = logprobsResult;
+            this.GroundingAttributions = groundingAttributions;
+            this.FinishMessage = finishMessage;
             this.AvgLogprobs = avgLogprobs;
-            this.Index = index;
+            this.UrlContextMetadata = urlContextMetadata;
+            this.FinishReason = finishReason;
         }
 
         /// <summary>
