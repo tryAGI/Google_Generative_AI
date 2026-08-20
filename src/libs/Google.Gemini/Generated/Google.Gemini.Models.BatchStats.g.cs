478b4fea959ab94868f67ba9a5852a5c7a7aa860
Konstantin S. <havendv@gmail.com>
2026-08-20 16:43:39 +0400

    feat: Updated OpenAPI spec (#921)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.BatchStats.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.BatchStats.g.cs
@@ -9,6 +9,20 @@ namespace Google.Gemini
     public sealed partial class BatchStats
     {
         /// <summary>
+        /// Output only. The number of requests in the batch.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("requestCount")]
+        public string? RequestCount { get; set; }
+
+        /// <summary>
+        /// Output only. The number of requests that are still pending processing.<br/>
+        /// Included only in responses
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("pendingRequestCount")]
+        public string? PendingRequestCount { get; set; }
+
+        /// <summary>
         /// Output only. The number of requests that were successfully processed.<br/>
         /// Included only in responses
         /// </summary>
@@ -23,20 +37,6 @@ namespace Google.Gemini
         public string? FailedRequestCount { get; set; }
 
         /// <summary>
-        /// Output only. The number of requests that are still pending processing.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("pendingRequestCount")]
-        public string? PendingRequestCount { get; set; }
-
-        /// <summary>
-        /// Output only. The number of requests in the batch.<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("requestCount")]
-        public string? RequestCount { get; set; }
-
-        /// <summary>
         /// Additional properties that are not explicitly defined in the schema
         /// </summary>
         [global::System.Text.Json.Serialization.JsonExtensionData]
@@ -45,6 +45,14 @@ namespace Google.Gemini
         /// <summary>
         /// Initializes a new instance of the <see cref="BatchStats" /> class.
         /// </summary>
+        /// <param name="requestCount">
+        /// Output only. The number of requests in the batch.<br/>
+        /// Included only in responses
+        /// </param>
+        /// <param name="pendingRequestCount">
+        /// Output only. The number of requests that are still pending processing.<br/>
+        /// Included only in responses
+        /// </param>
         /// <param name="successfulRequestCount">
         /// Output only. The number of requests that were successfully processed.<br/>
         /// Included only in responses
@@ -53,27 +61,19 @@ namespace Google.Gemini
         /// Output only. The number of requests that failed to be processed.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="pendingRequestCount">
-        /// Output only. The number of requests that are still pending processing.<br/>
-        /// Included only in responses
-        /// </param>
-        /// <param name="requestCount">
-        /// Output only. The number of requests in the batch.<br/>
-        /// Included only in responses
-        /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public BatchStats(
-            string? successfulRequestCount,
-            string? failedRequestCount,
+            string? requestCount,
             string? pendingRequestCount,
-            string? requestCount)
+            string? successfulRequestCount,
+            string? failedRequestCount)
         {
+            this.RequestCount = requestCount;
+            this.PendingRequestCount = pendingRequestCount;
             this.SuccessfulRequestCount = successfulRequestCount;
             this.FailedRequestCount = failedRequestCount;
-            this.PendingRequestCount = pendingRequestCount;
-            this.RequestCount = requestCount;
         }
 
         /// <summary>
--- src/libs/Google.Gemini/Generated/Google.Gemini.Models.Environment.g.cs
+++ src/libs/Google.Gemini/Generated/Google.Gemini.Models.Environment.g.cs
@@ -9,10 +9,11 @@ namespace Google.Gemini
     public sealed partial class Environment
     {
         /// <summary>
-        /// Network egress configuration for the environment.
+        /// Required. Output only. The ID of the environment.<br/>
+        /// Included only in responses
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("networkAllowlist")]
-        public global::Google.Gemini.EnvironmentNetworkEgressAllowlist? NetworkAllowlist { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("id")]
+        public string? Id { get; set; }
 
         /// <summary>
         /// Output only. The time at which the environment was last accessed in ISO 8601 format (YYYY-MM-DDThh:mm:ssZ).<br/>
@@ -29,13 +30,6 @@ namespace Google.Gemini
         public string? Updated { get; set; }
 
         /// <summary>
-        /// Output only. The time at which the environment was created in ISO 8601 format (YYYY-MM-DDThh:mm:ssZ).<br/>
-        /// Included only in responses
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("created")]
-        public string? Created { get; set; }
-
-        /// <summary>
         /// Output only. The total size of the environment files in bytes, output only.<br/>
         /// Included only in responses
         /// </summary>
@@ -43,11 +37,17 @@ namespace Google.Gemini
         public string? SizeBytes { get; set; }
 
         /// <summary>
-        /// Required. Output only. The ID of the environment.<br/>
+        /// Network egress configuration for the environment.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("networkAllowlist")]
+        public global::Google.Gemini.EnvironmentNetworkEgressAllowlist? NetworkAllowlist { get; set; }
+
+        /// <summary>
+        /// Output only. The time at which the environment was created in ISO 8601 format (YYYY-MM-DDThh:mm:ssZ).<br/>
         /// Included only in responses
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("id")]
-        public string? Id { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("created")]
+        public string? Created { get; set; }
 
         /// <summary>
         /// Output only. The status of the environment container.<br/>
@@ -58,10 +58,11 @@ namespace Google.Gemini
         public global::Google.Gemini.EnvironmentStatus? Status { get; set; }
 
         /// <summary>
-        /// Sources to be mounted into the environment.
+        /// Network egress mode.
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("sources")]
-        public global::System.Collections.Generic.IList<global::Google.Gemini.Source>? Sources { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("networkMode")]
+        [global::System.Text.Json.Serialization.JsonConverter(typeof(global::Google.Gemini.JsonConverters.EnvironmentNetworkModeJsonConverter))]
+        public global::Google.Gemini.EnvironmentNetworkMode? NetworkMode { get; set; }
 
         /// <summary>
         /// Output only. The number of files in the environment, output only.<br/>
@@ -71,11 +72,10 @@ namespace Google.Gemini
         public string? FileCount { get; set; }
 
         /// <summary>
-        /// Network egress mode.
+        /// Sources to be mounted into the environment.
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("networkMode")]
-        [global::System.Text.Json.Serialization.JsonConverter(typeof(global::Google.Gemini.JsonConverters.EnvironmentNetworkModeJsonConverter))]
-        public global::Google.Gemini.EnvironmentNetworkMode? NetworkMode { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("sources")]
+        public global::System.Collections.Generic.IList<global::Google.Gemini.Source>? Sources { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -86,8 +86,9 @@ namespace Google.Gemini
         /// <summary>
         /// Initializes a new instance of the <see cref="Environment" /> class.
         /// </summary>
-        /// <param name="networkAllowlist">
-        /// Network egress configuration for the environment.
+        /// <param name="id">
+        /// Required. Output only. The ID of the environment.<br/>
+        /// Included only in responses
         /// </param>
         /// <param name="lastAccessed">
         /// Output only. The time at which the environment was last accessed in ISO 8601 format (YYYY-MM-DDThh:mm:ssZ).<br/>
@@ -97,57 +98,56 @@ namespace Google.Gemini
         /// Output only. The time at which the environment was last updated in ISO 8601 format (YYYY-MM-DDThh:mm:ssZ).<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="created">
-        /// Output only. The time at which the environment was created in ISO 8601 format (YYYY-MM-DDThh:mm:ssZ).<br/>
-        /// Included only in responses
-        /// </param>
         /// <param name="sizeBytes">
         /// Output only. The total size of the environment files in bytes, output only.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="id">
-        /// Required. Output only. The ID of the environment.<br/>
+        /// <param name="networkAllowlist">
+        /// Network egress configuration for the environment.
+        /// </param>
+        /// <param name="created">
+        /// Output only. The time at which the environment was created in ISO 8601 format (YYYY-MM-DDThh:mm:ssZ).<br/>
         /// Included only in responses
         /// </param>
         /// <param name="status">
         /// Output only. The status of the environment container.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="sources">
-        /// Sources to be mounted into the environment.
+        /// <param name="networkMode">
+        /// Network egress mode.
         /// </param>
         /// <param name="fileCount">
         /// Output only. The number of files in the environment, output only.<br/>
         /// Included only in responses
         /// </param>
-        /// <param name="networkMode">
-        /// Network egress mode.
+        /// <param name="sources">
+        /// Sources to be mounted into the environment.
         /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public Environment(
-            global::Google.Gemini.EnvironmentNetworkEgressAllowlist? networkAllowlist,
+            string? id,
             string? lastAccessed,
             string? updated,
-            string? created,
             string? sizeBytes,
-            string? id,
+            global::Google.Gemini.EnvironmentNetworkEgressAllowlist? networkAllowlist,
+            string? created,
             global::Google.Gemini.EnvironmentStatus? status,
-            global::System.Collections.Generic.IList<global::Google.Gemini.Source>? sources,
+            global::Google.Gemini.EnvironmentNetworkMode? networkMode,
             string? fileCount,
-            global::Google.Gemini.EnvironmentNetworkMode? networkMode)
+            global::System.Collections.Generic.IList<global::Google.Gemini.Source>? sources)
         {
-            this.NetworkAllowlist = networkAllowlist;
+            this.Id = id;
             this.LastAccessed = lastAccessed;
             this.Updated = updated;
-            this.Created = created;
             this.SizeBytes = sizeBytes;
-            this.Id = id;
+            this.NetworkAllowlist = networkAllowlist;
+            this.Created = created;
             this.Status = status;
-            this.Sources = sources;
-            this.FileCount = fileCount;
             this.NetworkMode = networkMode;
+            this.FileCount = fileCount;
+            this.Sources = sources;
         }
 
         /// <summary>
